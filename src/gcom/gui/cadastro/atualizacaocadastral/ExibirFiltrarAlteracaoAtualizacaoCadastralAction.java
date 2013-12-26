2421d312c363cee037d94878b75c17ec220ce21d
Marcelo Garcia <marcelo.garcia@prodigasistemas.com.br>
2013-12-26 15:49:36 -0300

    Localidade

--- src/gcom/gui/cadastro/atualizacaocadastral/ExibirFiltrarAlteracaoAtualizacaoCadastralAction.java
+++ src/gcom/gui/cadastro/atualizacaocadastral/ExibirFiltrarAlteracaoAtualizacaoCadastralAction.java
@@ -1,84 +1,84 @@
 /*
-* Copyright (C) 2007-2007 the GSAN - Sistema Integrado de Gestão de Serviços de Saneamento
-*
-* This file is part of GSAN, an integrated service management system for Sanitation
-*
-* GSAN is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License.
-*
-* GSAN is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
-*/
+ * Copyright (C) 2007-2007 the GSAN - Sistema Integrado de Gestão de Serviços de Saneamento
+ *
+ * This file is part of GSAN, an integrated service management system for Sanitation
+ *
+ * GSAN is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * GSAN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
 
 /*
-* GSAN - Sistema Integrado de Gestão de Serviços de Saneamento
-* Copyright (C) <2007> 
-* Adriano Britto Siqueira
-* Alexandre Santos Cabral
-* Ana Carolina Alves Breda
-* Ana Maria Andrade Cavalcante
-* Aryed Lins de Araújo
-* Bruno Leonardo Rodrigues Barros
-* Carlos Elmano Rodrigues Ferreira
-* Cláudio de Andrade Lira
-* Denys Guimarães Guenes Tavares
-* Eduardo Breckenfeld da Rosa Borges
-* Fabíola Gomes de Araújo
-* Flávio Leonardo Cavalcanti Cordeiro
-* Francisco do Nascimento Júnior
-* Homero Sampaio Cavalcanti
-* Ivan Sérgio da Silva Júnior
-* José Edmar de Siqueira
-* José Thiago Tenório Lopes
-* Kássia Regina Silvestre de Albuquerque
-* Leonardo Luiz Vieira da Silva
-* Márcio Roberto Batista da Silva
-* Maria de Fátima Sampaio Leite
-* Micaela Maria Coelho de Araújo
-* Nelson Mendonça de Carvalho
-* Newton Morais e Silva
-* Pedro Alexandre Santos da Silva Filho
-* Rafael Corrêa Lima e Silva
-* Rafael Francisco Pinto
-* Rafael Koury Monteiro
-* Rafael Palermo de Araújo
-* Raphael Veras Rossiter
-* Roberto Sobreira Barbalho
-* Rodrigo Avellar Silveira
-* Rosana Carvalho Barbosa
-* Sávio Luiz de Andrade Cavalcante
-* Tai Mu Shih
-* Thiago Augusto Souza do Nascimento
-* Tiago Moreno Rodrigues
-* Vivianne Barbosa Sousa
-*
-* Este programa é software livre; você pode redistribuí-lo e/ou
-* modificá-lo sob os termos de Licença Pública Geral GNU, conforme
-* publicada pela Free Software Foundation; versão 2 da
-* Licença.
-* Este programa é distribuído na expectativa de ser útil, mas SEM
-* QUALQUER GARANTIA; sem mesmo a garantia implícita de
-* COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
-* PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
-* detalhes.
-* Você deve ter recebido uma cópia da Licença Pública Geral GNU
-* junto com este programa; se não, escreva para Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-* 02111-1307, USA.
-*/  
+ * GSAN - Sistema Integrado de Gestão de Serviços de Saneamento
+ * Copyright (C) <2007> 
+ * Adriano Britto Siqueira
+ * Alexandre Santos Cabral
+ * Ana Carolina Alves Breda
+ * Ana Maria Andrade Cavalcante
+ * Aryed Lins de Araújo
+ * Bruno Leonardo Rodrigues Barros
+ * Carlos Elmano Rodrigues Ferreira
+ * Cláudio de Andrade Lira
+ * Denys Guimarães Guenes Tavares
+ * Eduardo Breckenfeld da Rosa Borges
+ * Fabíola Gomes de Araújo
+ * Flávio Leonardo Cavalcanti Cordeiro
+ * Francisco do Nascimento Júnior
+ * Homero Sampaio Cavalcanti
+ * Ivan Sérgio da Silva Júnior
+ * José Edmar de Siqueira
+ * José Thiago Tenório Lopes
+ * Kássia Regina Silvestre de Albuquerque
+ * Leonardo Luiz Vieira da Silva
+ * Márcio Roberto Batista da Silva
+ * Maria de Fátima Sampaio Leite
+ * Micaela Maria Coelho de Araújo
+ * Nelson Mendonça de Carvalho
+ * Newton Morais e Silva
+ * Pedro Alexandre Santos da Silva Filho
+ * Rafael Corrêa Lima e Silva
+ * Rafael Francisco Pinto
+ * Rafael Koury Monteiro
+ * Rafael Palermo de Araújo
+ * Raphael Veras Rossiter
+ * Roberto Sobreira Barbalho
+ * Rodrigo Avellar Silveira
+ * Rosana Carvalho Barbosa
+ * Sávio Luiz de Andrade Cavalcante
+ * Tai Mu Shih
+ * Thiago Augusto Souza do Nascimento
+ * Tiago Moreno Rodrigues
+ * Vivianne Barbosa Sousa
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou
+ * modificá-lo sob os termos de Licença Pública Geral GNU, conforme
+ * publicada pela Free Software Foundation; versão 2 da
+ * Licença.
+ * Este programa é distribuído na expectativa de ser útil, mas SEM
+ * QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ * PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ * detalhes.
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa; se não, escreva para Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 package gcom.gui.cadastro.atualizacaocadastral;
 
-import gcom.cadastro.ArquivoTextoAtualizacaoCadastral;
-import gcom.cadastro.atualizacaocadastral.FiltroArquivoTextoAtualizacaoCadastral;
 import gcom.cadastro.empresa.Empresa;
 import gcom.cadastro.empresa.FiltroEmpresa;
+import gcom.cadastro.localidade.FiltroLocalidade;
+import gcom.cadastro.localidade.Localidade;
 import gcom.fachada.Fachada;
 import gcom.gui.ActionServletException;
 import gcom.gui.GcomAction;
@@ -88,9 +88,11 @@ import gcom.micromedicao.Leiturista;
 import gcom.seguranca.transacao.FiltroTabelaColuna;
 import gcom.seguranca.transacao.TabelaColuna;
 import gcom.util.ConstantesSistema;
+import gcom.util.Util;
 import gcom.util.filtro.ParametroSimples;
 import gcom.util.filtro.ParametroSimplesIn;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -104,61 +106,62 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.jboss.logging.Logger;
 
 public class ExibirFiltrarAlteracaoAtualizacaoCadastralAction extends GcomAction {
 
-    public ActionForward execute(ActionMapping actionMapping,
-            ActionForm actionForm, HttpServletRequest httpServletRequest,
-            HttpServletResponse httpServletResponse) {
+	private static Logger logger = Logger.getLogger(ExibirFiltrarAlteracaoAtualizacaoCadastralAction.class);
+	
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse) {
 
-        //Seta o retorno
-        ActionForward retorno = actionMapping
-                .findForward("exibirFiltrarAlteracaoAtualizacaoCadastral");
+		FiltrarAlteracaoAtualizacaoCadastralActionForm form = (FiltrarAlteracaoAtualizacaoCadastralActionForm) actionForm;
 
-        //Obtém a instância da fachada
-        Fachada fachada = Fachada.getInstancia();
+		ActionForward retorno = actionMapping.findForward("exibirFiltrarAlteracaoAtualizacaoCadastral");
 
-        //Obtém a sessão
-        HttpSession sessao = httpServletRequest.getSession(false);
-        
-        FiltrarAlteracaoAtualizacaoCadastralActionForm form = (FiltrarAlteracaoAtualizacaoCadastralActionForm) actionForm;
-        
+		Fachada fachada = Fachada.getInstancia();
+		
+		try {
+			if (httpServletRequest.getParameterMap().containsKey("filtroLocalidade")){
+				String filtro = httpServletRequest.getParameter("filtroLocalidade");
+				preencherCampoLocalidade(form, filtro, httpServletRequest);
+			}
+		} catch (Exception e) {
+			logger.error("Erro ao filtrar Cadastro", e);
+		}
+			
+
+		// Obtém a sessão
+		HttpSession sessao = httpServletRequest.getSession(false);
+
+
 		Collection colecaoLeiturista = new ArrayList();
-        
-		if(sessao.getAttribute("colecaoEmpresa") == null){
+
+		if (sessao.getAttribute("colecaoEmpresa") == null) {
 			FiltroEmpresa filtroEmpresa = new FiltroEmpresa();
-			filtroEmpresa.adicionarParametro(new ParametroSimples(
-					FiltroEmpresa.INDICADORUSO, ConstantesSistema.INDICADOR_USO_ATIVO));
-			
+			filtroEmpresa.adicionarParametro(new ParametroSimples(FiltroEmpresa.INDICADORUSO, ConstantesSistema.INDICADOR_USO_ATIVO));
+
 			filtroEmpresa.setCampoOrderBy(FiltroEmpresa.DESCRICAO);
-			
+
 			Collection<Empresa> colecaoEmpresa = fachada.pesquisar(filtroEmpresa, Empresa.class.getName());
-			
+
 			// [FS0001 - Verificar Existencia de dados]
-			if ( (colecaoEmpresa == null) || (colecaoEmpresa.size() == 0) ) {
-				throw new ActionServletException(
-						"atencao.entidade_sem_dados_para_selecao", null, Empresa.class.getName());
-			}else {
+			if ((colecaoEmpresa == null) || (colecaoEmpresa.size() == 0)) {
+				throw new ActionServletException("atencao.entidade_sem_dados_para_selecao", null, Empresa.class.getName());
+			} else {
 				sessao.setAttribute("colecaoEmpresa", colecaoEmpresa);
 			}
 		}
-		
+
 		// Leiturista da Empresa
-		if (form.getIdEmpresa() != null && !form.getIdEmpresa().equals("-1")
-				&& !form.getIdEmpresa().equals("")) {
+		if (form.getIdEmpresa() != null && !form.getIdEmpresa().equals("-1") && !form.getIdEmpresa().equals("")) {
 
-			FiltroLeiturista filtroLeiturista = new FiltroLeiturista(
-					FiltroLeiturista.ID);
-			filtroLeiturista.adicionarParametro(new ParametroSimples(
-					FiltroLeiturista.EMPRESA_ID,
-					form.getIdEmpresa()));
-			filtroLeiturista
-					.adicionarCaminhoParaCarregamentoEntidade(FiltroLeiturista.CLIENTE);
-			filtroLeiturista
-					.adicionarCaminhoParaCarregamentoEntidade(FiltroLeiturista.FUNCIONARIO);
+			FiltroLeiturista filtroLeiturista = new FiltroLeiturista(FiltroLeiturista.ID);
+			filtroLeiturista.adicionarParametro(new ParametroSimples(FiltroLeiturista.EMPRESA_ID, form.getIdEmpresa()));
+			filtroLeiturista.adicionarCaminhoParaCarregamentoEntidade(FiltroLeiturista.CLIENTE);
+			filtroLeiturista.adicionarCaminhoParaCarregamentoEntidade(FiltroLeiturista.FUNCIONARIO);
 
-			Collection colecao = fachada.pesquisar(filtroLeiturista,
-					Leiturista.class.getName());
+			Collection colecao = fachada.pesquisar(filtroLeiturista, Leiturista.class.getName());
 
 			if (colecao != null && !colecao.isEmpty()) {
 				Iterator it = colecao.iterator();
@@ -166,125 +169,113 @@ public class ExibirFiltrarAlteracaoAtualizacaoCadastralAction extends GcomAction
 					Leiturista leitu = (Leiturista) it.next();
 					DadosLeiturista dadosLeiu = null;
 					if (leitu.getFuncionario() != null) {
-						dadosLeiu = new DadosLeiturista(leitu.getId(), leitu
-								.getFuncionario().getNome());
+						dadosLeiu = new DadosLeiturista(leitu.getId(), leitu.getFuncionario().getNome());
 					} else {
-						dadosLeiu = new DadosLeiturista(leitu.getId(), leitu
-								.getCliente().getNome());
+						dadosLeiu = new DadosLeiturista(leitu.getId(), leitu.getCliente().getNome());
 					}
 					colecaoLeiturista.add(dadosLeiu);
 				}
 			}
 
 		}
-		
+
 		sessao.setAttribute("colecaoLeiturista", colecaoLeiturista);
-		
-		
-		String idDigitadoArquivo = form.getIdArquivo();
-        
-		// Verifica se o código foi digitado
-		if (idDigitadoArquivo != null && !idDigitadoArquivo.trim().equals("")) {
-			FiltroArquivoTextoAtualizacaoCadastral filtroArquivoTextoAtualizacaoCadastral = new FiltroArquivoTextoAtualizacaoCadastral();
 
-			filtroArquivoTextoAtualizacaoCadastral.adicionarParametro(new ParametroSimples(
-					FiltroArquivoTextoAtualizacaoCadastral.ID, idDigitadoArquivo));
-
-			Collection arquivosTextoAtualizacaoCadastral = fachada.pesquisar(filtroArquivoTextoAtualizacaoCadastral,
-					ArquivoTextoAtualizacaoCadastral.class.getName());
-
-			if (arquivosTextoAtualizacaoCadastral != null && !arquivosTextoAtualizacaoCadastral.isEmpty()) {
-				form.setIdArquivo("" + ((ArquivoTextoAtualizacaoCadastral) ((List) arquivosTextoAtualizacaoCadastral).get(0))
-					.getId());
-				form.setDescricaoArquivo(((ArquivoTextoAtualizacaoCadastral) ((List) arquivosTextoAtualizacaoCadastral).get(0))
-					.getDescricaoArquivo());
-				httpServletRequest.setAttribute("idArquivoEncontrado","true");
-				//httpServletRequest.setAttribute("nomeCampo","idCliente");
-
-			} else {
-
-				form.setIdArquivo("");
-				//httpServletRequest.setAttribute("idClienteNaoEncontrado",
-					//	"exception");
-				form.setDescricaoArquivo("ARQUIVO INEXISTENTE");
-				//httpServletRequest.setAttribute("nomeCampo","DDDTelefone");
-
-			}
-
-		}
-		
 		Collection colecaoColunaImoveisSelecionados = null;
-		
-		if(form.getColunaImoveisSelecionados() != null){
-			
+
+		if (form.getColunaImoveisSelecionados() != null) {
+
 			String[] aux = form.getColunaImoveisSelecionados();
-			
+
 			List aux1 = Arrays.asList(aux);
 			colecaoColunaImoveisSelecionados = aux1;
-			
+
 			FiltroTabelaColuna filtroTabelaColuna = new FiltroTabelaColuna();
-			
-			filtroTabelaColuna.adicionarParametro(
-					new ParametroSimplesIn(
-							FiltroTabelaColuna.ID, colecaoColunaImoveisSelecionados));
-			
+
+			filtroTabelaColuna.adicionarParametro(new ParametroSimplesIn(FiltroTabelaColuna.ID, colecaoColunaImoveisSelecionados));
+
 			filtroTabelaColuna.setCampoOrderBy(FiltroTabelaColuna.DESCRICAO_COLUNA);
-			
+
 			// Pesquisa de acordo com os parâmetros informados no filtro
-			colecaoColunaImoveisSelecionados = Fachada.getInstancia().pesquisar(
-					filtroTabelaColuna, TabelaColuna.class.getName());
-			
-			
+			colecaoColunaImoveisSelecionados = Fachada.getInstancia().pesquisar(filtroTabelaColuna, TabelaColuna.class.getName());
+
 			// Verifica se a pesquisa retornou algum objeto para a coleção
 			if (colecaoColunaImoveisSelecionados != null && !colecaoColunaImoveisSelecionados.isEmpty()) {
 				sessao.setAttribute("colecaoColunaImoveisSelecionados", colecaoColunaImoveisSelecionados);
 				sessao.setAttribute("existeColecaoColunaImoveisSelecionados", colecaoColunaImoveisSelecionados);
 			}
 		}
-		
-//		 Monta a colecao 
-		this.pesquisarColunaImoveis(httpServletRequest, colecaoColunaImoveisSelecionados);
-	
-        return retorno;
-    }
-    
-    private void pesquisarColunaImoveis(HttpServletRequest httpServletRequest, Collection colecaoColunaImoveisSelecionados) {
 
-    	HttpSession sessao = httpServletRequest.getSession(false);
-    	
-    	FiltroTabelaColuna filtroTabelaColuna = new FiltroTabelaColuna();
-		
-		filtroTabelaColuna.adicionarParametro(
-				new ParametroSimples(
-						FiltroTabelaColuna.INDICADOR_ATUALIZACAO_CADASRAL, ConstantesSistema.SIM));
-		
+		// Monta a colecao
+		this.pesquisarColunaImoveis(httpServletRequest, colecaoColunaImoveisSelecionados);
+
+		return retorno;
+	}
+
+	private void pesquisarColunaImoveis(HttpServletRequest httpServletRequest, Collection colecaoColunaImoveisSelecionados) {
+		HttpSession sessao = httpServletRequest.getSession(false);
+
+		FiltroTabelaColuna filtroTabelaColuna = new FiltroTabelaColuna();
+
+		filtroTabelaColuna.adicionarParametro(new ParametroSimples(FiltroTabelaColuna.INDICADOR_ATUALIZACAO_CADASRAL, ConstantesSistema.SIM));
+
 		filtroTabelaColuna.setConsultaSemLimites(true);
 		filtroTabelaColuna.setCampoOrderBy(FiltroTabelaColuna.DESCRICAO_COLUNA);
-        
+
 		Collection colecaoColunaImoveis = Fachada.getInstancia().pesquisar(filtroTabelaColuna, TabelaColuna.class.getName());
 
-			if(colecaoColunaImoveisSelecionados == null ){
-				sessao.setAttribute("colecaoColunaImoveis",
-						colecaoColunaImoveis);
-			}else{
-				for (Iterator iteratorColunaImoveis = colecaoColunaImoveis.iterator(); iteratorColunaImoveis.hasNext();){
-			
-					TabelaColuna colunaImoveis = (TabelaColuna) iteratorColunaImoveis.next();
-					for (Iterator iteratorColunaImoveisSelecionados = colecaoColunaImoveisSelecionados.iterator(); 
-					iteratorColunaImoveisSelecionados.hasNext();){
-						
-						TabelaColuna colunaImoveisSelecionado = (TabelaColuna) iteratorColunaImoveisSelecionados.next();
-						
-						if(colunaImoveis.getId().compareTo(colunaImoveisSelecionado.getId()) == 0){
-							iteratorColunaImoveis.remove();
-						}
+		if (colecaoColunaImoveisSelecionados == null) {
+			sessao.setAttribute("colecaoColunaImoveis", colecaoColunaImoveis);
+		} else {
+			for (Iterator iteratorColunaImoveis = colecaoColunaImoveis.iterator(); iteratorColunaImoveis.hasNext();) {
+
+				TabelaColuna colunaImoveis = (TabelaColuna) iteratorColunaImoveis.next();
+				for (Iterator iteratorColunaImoveisSelecionados = colecaoColunaImoveisSelecionados.iterator(); iteratorColunaImoveisSelecionados.hasNext();) {
+
+					TabelaColuna colunaImoveisSelecionado = (TabelaColuna) iteratorColunaImoveisSelecionados.next();
+
+					if (colunaImoveis.getId().compareTo(colunaImoveisSelecionado.getId()) == 0) {
+						iteratorColunaImoveis.remove();
 					}
 				}
-
-				sessao.setAttribute("colecaoColunaImoveis", colecaoColunaImoveis);
 			}
-//		}
-	}
-    
-}
 
+			sessao.setAttribute("colecaoColunaImoveis", colecaoColunaImoveis);
+		}
+	}
+
+	private void preencherCampoLocalidade(FiltrarAlteracaoAtualizacaoCadastralActionForm form, String objetoPesquisa, HttpServletRequest httpServletRequest)
+			throws Exception {
+
+		Method getId = FiltrarAlteracaoAtualizacaoCadastralActionForm.class.getMethod("getId" + objetoPesquisa);
+
+		Method setId = FiltrarAlteracaoAtualizacaoCadastralActionForm.class.getMethod("setId" + objetoPesquisa, String.class);
+
+		Method setNome = FiltrarAlteracaoAtualizacaoCadastralActionForm.class.getMethod("setNome" + objetoPesquisa, String.class);
+
+		String idLocalidade = (String) getId.invoke(form);
+		Localidade localidade = pesquisarLocalidade(idLocalidade);
+
+		if (localidade == null) {
+			setId.invoke(form, "");
+			setNome.invoke(form, "Localidade Inexistente");
+			httpServletRequest.setAttribute(objetoPesquisa + "Inexistente", "true");
+		} else {
+			setNome.invoke(form, localidade.getDescricao());
+		}
+	}
+
+	private Localidade pesquisarLocalidade(String idLocalidade) {
+		FiltroLocalidade filtroLocalidade = new FiltroLocalidade();
+		filtroLocalidade.adicionarParametro(new ParametroSimples(FiltroLocalidade.ID, idLocalidade));
+		filtroLocalidade.adicionarParametro(new ParametroSimples(FiltroLocalidade.INDICADORUSO, ConstantesSistema.INDICADOR_USO_ATIVO));
+
+		Collection colecaoPesquisa = Fachada.getInstancia().pesquisar(filtroLocalidade, Localidade.class.getName());
+
+		if (colecaoPesquisa == null || colecaoPesquisa.isEmpty()) {
+			return null;
+		} else {
+			return (Localidade) Util.retonarObjetoDeColecao(colecaoPesquisa);
+		}
+	}
+}
