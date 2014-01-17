acf2b4135def6462d64131be368b4f25e973c985
Pamela Gatinho <pamelagatinho@gmail.com>
2014-01-17 11:12:26 -0300

    Salvando ImovelRetorno da atualizacao cadastral

---
+++ src/gcom/atualizacaocadastral/ControladorAtualizacaoCadastralLocalHome.java
@@ -0,0 +1,9 @@
+package gcom.atualizacaocadastral;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBLocalHome;
+
+public interface ControladorAtualizacaoCadastralLocalHome  extends EJBLocalHome{
+
+	public ControladorAtualizacaoCadastralLocal create() throws CreateException;
+}
