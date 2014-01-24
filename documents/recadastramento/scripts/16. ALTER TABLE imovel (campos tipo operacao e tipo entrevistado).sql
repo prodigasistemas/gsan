0e7b57d0da805bd54e452ce4df6d6f6f7a3ce63e
Paulo Igor <piagodinho@gmail.com>
2014-01-24 17:49:22 -0300

    Resolvendo conflitos do merge com a master

--- documents/recadastramento/scripts/16. ALTER TABLE imovel (campos tipo operacao e tipo entrevistado).sql
+++ documents/recadastramento/scripts/16. ALTER TABLE imovel (campos tipo operacao e tipo entrevistado).sql
@@ -1,5 +1,4 @@
 ALTER TABLE cadastro.imovel_atlz_cadastral RENAME COLUMN imac_nmentrevistado TO imac_tipoentrevistado;
-<<<<<<< HEAD
 
 UPDATE seguranca.tabela_coluna
    SET tbco_nmcoluna='imac_tipoentrevistado', tbco_dscoluna = 'Entrevistado'
@@ -8,7 +7,3 @@ UPDATE seguranca.tabela_coluna
 ALTER TABLE cadastro.imovel_atlz_cadastral ADD COLUMN imac_tipooperacao INTEGER;
 ALTER TABLE atualizacaocadastral.imovel_retorno RENAME COLUMN imac_nmentrevistado TO imac_tipoentrevistado;
 ALTER TABLE atualizacaocadastral.imovel_retorno ADD COLUMN imac_tipooperacao INTEGER;
-=======
-ALTER TABLE cadastro.imovel_atlz_cadastral ADD COLUMN imac_tipooperacao INTEGER;
-ALTER TABLE atualizacaocadastral.imovel_retorno RENAME COLUMN imac_nmentrevistado TO imac_tipoentrevistado;
->>>>>>> master
