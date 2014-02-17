ce01832344f5b59545a6d924106911a2db0f3484
Felipe <felipe.santos@prodigasistemas.com.br>
2014-02-17 10:53:18 -0300

    Recadastramento - Atualizando scripts

---
+++ documents/recadastramento/scripts/30. ALTER TABLE atualizacaocadastral.imovel_controle_atlz_cad.sql
@@ -0,0 +1,7 @@
+ALTER TABLE atualizacaocadastral.imovel_controle_atlz_cad  DROP CONSTRAINT fk1_imovel_controle_atlz_cad;
+
+ALTER TABLE atualizacaocadastral.imovel_controle_atlz_cad ALTER COLUMN imov_id DROP NOT NULL;
+
+ALTER TABLE atualizacaocadastral.imovel_controle_atlz_cad ADD COLUMN imre_id INTEGER CONSTRAINT fk1_imovel_controle_atlz_cad
+REFERENCES atualizacaocadastral.imovel_retorno (imre_id) MATCH SIMPLE
+ON UPDATE RESTRICT ON DELETE RESTRICT;
