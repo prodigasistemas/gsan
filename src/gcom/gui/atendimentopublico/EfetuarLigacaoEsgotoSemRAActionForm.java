534f557571e085d1be42cf261b6100fd955a131d
Adriana <adrianasantanamuniz@gmail.com>
2014-03-18 10:24:06 -0300

    Refatoração

--- src/gcom/gui/atendimentopublico/EfetuarLigacaoEsgotoSemRAActionForm.java
+++ src/gcom/gui/atendimentopublico/EfetuarLigacaoEsgotoSemRAActionForm.java
@@ -79,37 +79,28 @@ import org.apache.struts.action.ActionForm;
 
 public class EfetuarLigacaoEsgotoSemRAActionForm extends ActionForm{
 	private static final long serialVersionUID = 1L;
-	//Imóvel
     private String matriculaImovel;
     private String inscricaoImovel;
     private String clienteUsuario;
     private String cpfCnpjCliente;
     private String situacaoLigacaoAgua;
     private String situacaoLigacaoEsgoto;
-    //Dados da Ligação
     private String diametroLigacao;
     private String dataLigacao;
     private String materialLigacao;
     private String perfilLigacao;
     private String percentualColeta;
     private String percentualEsgoto;
-    
-    private String indicadorCaixaGordura;  
-    
-//  Dados da Geração do Débito
+    private String indicadorCaixaGordura; 
+    private String indicadorLigacao;
     private String idTipoDebito;
     private String descricaoTipoDebito;
-    
     private String valorDebito;
     private String motivoNaoCobranca;
     private String percentualCobranca;
     private String quantidadeParcelas;
     private String valorParcelas;
     
-    /*
-	 * Colocado por Raphael Rossiter em 18/04/2007
-	 * [FS0013 - Alteração de Valor]
-	 */
     private String alteracaoValor;
     
     public String getDescricaoTipoDebito() {
@@ -154,177 +145,94 @@ public class EfetuarLigacaoEsgotoSemRAActionForm extends ActionForm{
 	public void setValorParcelas(String valorParcelas) {
 		this.valorParcelas = valorParcelas;
 	}
-	/**
-	 * @return Retorna o campo cpfCnpjCliente.
-	 */
 	public String getCpfCnpjCliente() {
 		return cpfCnpjCliente;
 	}
-	/**
-	 * @param cpfCnpjCliente O cpfCnpjCliente a ser setado.
-	 */
 	public void setCpfCnpjCliente(String cpfCnpjCliente) {
 		this.cpfCnpjCliente = cpfCnpjCliente;
 	}
-	/**
-	 * @return Retorna o campo dataLigacao.
-	 */
 	public String getDataLigacao() {
 		return dataLigacao;
 	}
-	/**
-	 * @param dataLigacao O dataLigacao a ser setado.
-	 */
 	public void setDataLigacao(String dataLigacao) {
 		this.dataLigacao = dataLigacao;
 	}
-
-
-	/**
-	 * @return Retorna o campo inscricaoImovel.
-	 */
 	public String getInscricaoImovel() {
 		return inscricaoImovel;
 	}
-	/**
-	 * @param inscricaoImovel O inscricaoImovel a ser setado.
-	 */
 	public void setInscricaoImovel(String inscricaoImovel) {
 		this.inscricaoImovel = inscricaoImovel;
 	}
-	/**
-	 * @return Retorna o campo matriculaImovel.
-	 */
 	public String getMatriculaImovel() {
 		return matriculaImovel;
 	}
-	/**
-	 * @param matriculaImovel O matriculaImovel a ser setado.
-	 */
 	public void setMatriculaImovel(String matriculaImovel) {
 		this.matriculaImovel = matriculaImovel;
 	}
-	
-	/**
-	 * @return Retorna o campo situacaoLigacaoAgua.
-	 */
 	public String getSituacaoLigacaoAgua() {
 		return situacaoLigacaoAgua;
 	}
-	/**
-	 * @param situacaoLigacaoAgua O situacaoLigacaoAgua a ser setado.
-	 */
 	public void setSituacaoLigacaoAgua(String situacaoLigacaoAgua) {
 		this.situacaoLigacaoAgua = situacaoLigacaoAgua;
 	}
-	/**
-	 * @return Retorna o campo situacaoLigacaoEsgoto.
-	 */
 	public String getSituacaoLigacaoEsgoto() {
 		return situacaoLigacaoEsgoto;
 	}
-	/**
-	 * @param situacaoLigacaoEsgoto O situacaoLigacaoEsgoto a ser setado.
-	 */
 	public void setSituacaoLigacaoEsgoto(String situacaoLigacaoEsgoto) {
 		this.situacaoLigacaoEsgoto = situacaoLigacaoEsgoto;
 	}
-	/**
-	 * @return Retorna o campo clienteUsuario.
-	 */
 	public String getClienteUsuario() {
 		return clienteUsuario;
 	}
-	/**
-	 * @param clienteUsuario O clienteUsuario a ser setado.
-	 */
 	public void setClienteUsuario(String clienteUsuario) {
 		this.clienteUsuario = clienteUsuario;
 	}
-	
-	/**
-	 * @param diametroLigacao O diametroLigacao a ser setado.
-	 */
 	public void setDiametroLigacao(String diametroLigacao) {
 		this.diametroLigacao = diametroLigacao;
 	}
-	/**
-	 * @return Retorna o campo materialLigacao.
-	 */
 	public String getMaterialLigacao() {
 		return materialLigacao;
 	}
-	/**
-	 * @param materialLigacao O materialLigacao a ser setado.
-	 */
 	public void setMaterialLigacao(String materialLigacao) {
 		this.materialLigacao = materialLigacao;
 	}
-	/**
-	 * @return Retorna o campo perfilLigacao.
-	 */
 	public String getPerfilLigacao() {
 		return perfilLigacao;
 	}
-	/**
-	 * @param perfilLigacao O perfilLigacao a ser setado.
-	 */
 	public void setPerfilLigacao(String perfilLigacao) {
 		this.perfilLigacao = perfilLigacao;
 	}
-
-	/**
-	 * @return Retorna o campo percentualColeta.
-	 */
 	public String getPercentualColeta() {
 		return percentualColeta;
 	}
-	/**
-	 * @param percentualColeta O percentualColeta a ser setado.
-	 */
 	public void setPercentualColeta(String percentualColeta) {
 		this.percentualColeta = percentualColeta;
 	}
-	/**
-	 * @return Retorna o campo percentualEsgoto.
-	 */
 	public String getPercentualEsgoto() {
 		return percentualEsgoto;
 	}
-	/**
-	 * @param percentualEsgoto O percentualEsgoto a ser setado.
-	 */
 	public void setPercentualEsgoto(String percentualEsgoto) {
 		this.percentualEsgoto = percentualEsgoto;
 	}
-	/**
-	 * @return Retorna o campo diametroLigacao.
-	 */
 	public String getDiametroLigacao() {
 		return diametroLigacao;
 	}
-	
-	/**
-	 * @return Retorna o campo indicadorCaixaGordura.
-	 */
 	public String getIndicadorCaixaGordura() {
 		return indicadorCaixaGordura;
 	}
-	/**
-	 * @param indicadorCaixaGordura O indicadorCaixaGordura a ser setado.
-	 */
 	public void setIndicadorCaixaGordura(String indicadorCaixaGordura) {
 		this.indicadorCaixaGordura = indicadorCaixaGordura;
 	}
-	
 	public String getAlteracaoValor() {
 		return alteracaoValor;
 	}
 	public void setAlteracaoValor(String alteracaoValor) {
 		this.alteracaoValor = alteracaoValor;
 	}
-   
-	
-	
-
+	public String getIndicadorLigacao() {
+		return indicadorLigacao;
+	}
+	public void setIndicadorLigacao(String indicadorLigacao) {
+		this.indicadorLigacao = indicadorLigacao;
+	}
 }
--- src/gcom/gui/atendimentopublico/ExibirAlterarSituacaoLigacaoAction.java
+++ src/gcom/gui/atendimentopublico/ExibirAlterarSituacaoLigacaoAction.java
@@ -123,8 +123,7 @@ public class ExibirAlterarSituacaoLigacaoAction extends GcomAction {
 
 					IntegracaoQuadraFaceHelper integracao = fachada.integracaoQuadraFace(imovel.getId());					indicadorRedeAgua = integracao.getIndicadorRedeAgua().toString();					indicadorRedeEsgoto = integracao.getIndicadorRedeEsgoto().toString();					this.pesquisarSelectObrigatorio(httpServletRequest, form, indicadorRedeAgua, indicadorRedeEsgoto);
 					this.pesquisarCliente(form, new Integer(matriculaImovel));				}			} else {				httpServletRequest.setAttribute("OrdemServicoInexistente", true);				form.setIdOrdemServico("");				form.setNomeOrdemServico("ORDEM DE SERVIÇO INEXISTENTE");			}		}		return retorno;	}
-	private void pesquisarCliente(AlterarSituacaoLigacaoActionForm form, Integer matriculaImovel) {
-		FiltroClienteImovel filtroClienteImovel = new FiltroClienteImovel();		filtroClienteImovel.adicionarParametro(new ParametroSimples(FiltroClienteImovel.IMOVEL_ID, matriculaImovel));		filtroClienteImovel.adicionarParametro(new ParametroSimples(FiltroClienteImovel.CLIENTE_RELACAO_TIPO,				new Integer(ClienteRelacaoTipo.USUARIO)));		filtroClienteImovel.adicionarParametro(new ParametroNulo(FiltroClienteImovel.DATA_FIM_RELACAO));		filtroClienteImovel.adicionarCaminhoParaCarregamentoEntidade("cliente");		Collection colecaoClienteImovel = Fachada.getInstancia().pesquisar(filtroClienteImovel, ClienteImovel.class.getName());
+	private void pesquisarCliente(AlterarSituacaoLigacaoActionForm form, Integer matriculaImovel) {		FiltroClienteImovel filtroClienteImovel = new FiltroClienteImovel();		filtroClienteImovel.adicionarParametro(new ParametroSimples(FiltroClienteImovel.IMOVEL_ID, matriculaImovel));		filtroClienteImovel.adicionarParametro(new ParametroSimples(FiltroClienteImovel.CLIENTE_RELACAO_TIPO,				new Integer(ClienteRelacaoTipo.USUARIO)));		filtroClienteImovel.adicionarParametro(new ParametroNulo(FiltroClienteImovel.DATA_FIM_RELACAO));		filtroClienteImovel.adicionarCaminhoParaCarregamentoEntidade("cliente");		Collection colecaoClienteImovel = Fachada.getInstancia().pesquisar(filtroClienteImovel, ClienteImovel.class.getName());
 		if (colecaoClienteImovel != null && !colecaoClienteImovel.isEmpty()) {			ClienteImovel clienteImovel = (ClienteImovel) colecaoClienteImovel.iterator().next();			Cliente cliente = clienteImovel.getCliente();			String documento = "";
 			if (cliente.getCpf() != null && !cliente.getCpf().equals("")) {				documento = cliente.getCpfFormatado();			} else {				documento = cliente.getCnpjFormatado();			}
 			form.setClienteUsuario(cliente.getNome());			form.setCpfCnpjCliente(documento);		} else {			throw new ActionServletException("atencao.naocadastrado", null, "Cliente");		}	}
@@ -133,40 +132,34 @@ public class ExibirAlterarSituacaoLigacaoAction extends GcomAction {
 		Fachada fachada = Fachada.getInstancia();		FiltroLigacaoAguaSituacao filtroLigacaoAguaSituacao = new FiltroLigacaoAguaSituacao();		FiltroLigacaoEsgotoSituacao filtroLigacaoEsgotoSituacao = new FiltroLigacaoEsgotoSituacao();		
 		httpServletRequest.setAttribute("colecaoLigacaoAguaSituacao", new ArrayList());		httpServletRequest.setAttribute("colecaoLigacaoEsgotoSituacao",new ArrayList());
 		if (form.getIndicadorTipoLigacao() != null) {			if (form.getIndicadorTipoLigacao().equals("1")	|| form.getIndicadorTipoLigacao().equals("3")) {				if (indicadorRedeAgua != null) {
-					httpServletRequest.setAttribute("comboLigacaoAgua", "sim");					if (indicadorRedeAgua.equalsIgnoreCase("" + Quadra.SEM_REDE)) {
-						filtroLigacaoAguaSituacao.setCampoOrderBy(FiltroLigacaoAguaSituacao.DESCRICAO);						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoAguaSituacao.ID, LigacaoAguaSituacao.POTENCIAL));
-						Collection<LigacaoAguaSituacao> colecaoLigacaoAguaSituacao = fachada.pesquisar(filtroLigacaoAguaSituacao,										LigacaoAguaSituacao.class.getName());
-						if (colecaoLigacaoAguaSituacao == null	|| colecaoLigacaoAguaSituacao.isEmpty()) {							throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",									null, "Ligacao Agua Situacao ");						}
-						httpServletRequest.setAttribute("colecaoLigacaoAguaSituacao", colecaoLigacaoAguaSituacao);					}
-					if (indicadorRedeAgua.equalsIgnoreCase("" + Quadra.COM_REDE)) {
+					Collection<LigacaoAguaSituacao> colecaoLigacaoAguaSituacao = null;
+					httpServletRequest.setAttribute("comboLigacaoAgua", "sim");
+										if (indicadorRedeAgua.equalsIgnoreCase("" + Quadra.SEM_REDE)) {						filtroLigacaoAguaSituacao.setCampoOrderBy(FiltroLigacaoAguaSituacao.DESCRICAO);						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoAguaSituacao.ID, LigacaoAguaSituacao.POTENCIAL));						colecaoLigacaoAguaSituacao = fachada.pesquisar(filtroLigacaoAguaSituacao, LigacaoAguaSituacao.class.getName());					}					if (indicadorRedeAgua.equalsIgnoreCase("" + Quadra.COM_REDE)) {
 						filtroLigacaoAguaSituacao.setCampoOrderBy(FiltroLigacaoAguaSituacao.DESCRICAO);						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoAguaSituacao.ID, LigacaoAguaSituacao.FACTIVEL));
 						filtroLigacaoAguaSituacao.adicionarParametro( new ParametroSimples(
-								FiltroLigacaoAguaSituacao.INDICADOR_FATURAMENTO, ConstantesSistema.NAO ));						
-						Collection<LigacaoAguaSituacao> colecaoLigacaoAguaSituacao = fachada								.pesquisar(filtroLigacaoAguaSituacao, LigacaoAguaSituacao.class.getName());
-						if (colecaoLigacaoAguaSituacao == null || colecaoLigacaoAguaSituacao.isEmpty()) {							throw new ActionServletException( "atencao.entidade_sem_dados_para_selecao",									null, "Ligacao Agua Situacao ");						}
-						httpServletRequest.setAttribute("colecaoLigacaoAguaSituacao", colecaoLigacaoAguaSituacao);					}
-					if (indicadorRedeAgua.equalsIgnoreCase(""+ Quadra.REDE_PARCIAL)) {
-						filtroLigacaoAguaSituacao.setCampoOrderBy(FiltroLigacaoAguaSituacao.DESCRICAO);						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(FiltroLigacaoAguaSituacao.ID,								LigacaoAguaSituacao.POTENCIAL, ConectorOr.CONECTOR_OR ));						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(FiltroLigacaoAguaSituacao.ID,								LigacaoAguaSituacao.FACTIVEL, ConectorAnd.CONECTOR_AND, 2));
+								FiltroLigacaoAguaSituacao.INDICADOR_FATURAMENTO, ConstantesSistema.NAO ));												colecaoLigacaoAguaSituacao = fachada.pesquisar(filtroLigacaoAguaSituacao, LigacaoAguaSituacao.class.getName());
+					}					if (indicadorRedeAgua.equalsIgnoreCase(""+ Quadra.REDE_PARCIAL)) {						filtroLigacaoAguaSituacao.setCampoOrderBy(FiltroLigacaoAguaSituacao.DESCRICAO);						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(FiltroLigacaoAguaSituacao.ID,								LigacaoAguaSituacao.POTENCIAL, ConectorOr.CONECTOR_OR ));						filtroLigacaoAguaSituacao.adicionarParametro(new ParametroSimples(FiltroLigacaoAguaSituacao.ID,								LigacaoAguaSituacao.FACTIVEL, ConectorAnd.CONECTOR_AND, 2));
 						filtroLigacaoAguaSituacao.adicionarParametro( new ParametroSimples(
-								FiltroLigacaoAguaSituacao.INDICADOR_FATURAMENTO, ConstantesSistema.NAO ) );													Collection<LigacaoAguaSituacao> colecaoLigacaoAguaSituacao = fachada								.pesquisar(filtroLigacaoAguaSituacao, LigacaoAguaSituacao.class.getName());						if (colecaoLigacaoAguaSituacao == null || colecaoLigacaoAguaSituacao.isEmpty()) {							throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",									null, "Tabela Ligacao Agua Situacao ");						}
-						httpServletRequest.setAttribute("colecaoLigacaoAguaSituacao",								colecaoLigacaoAguaSituacao);					}				}			}
+								FiltroLigacaoAguaSituacao.INDICADOR_FATURAMENTO, ConstantesSistema.NAO ) );													colecaoLigacaoAguaSituacao = fachada.pesquisar(filtroLigacaoAguaSituacao, LigacaoAguaSituacao.class.getName());					}
+					
+										if (colecaoLigacaoAguaSituacao == null || colecaoLigacaoAguaSituacao.isEmpty()) {						throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",								null, "Tabela Ligacao Agua Situacao ");					}					
+					httpServletRequest.setAttribute("colecaoLigacaoAguaSituacao", colecaoLigacaoAguaSituacao);				}			}
 
-			if (form.getIndicadorTipoLigacao().equals("2") || form.getIndicadorTipoLigacao().equals("3")) {				if (indicadorRedeEsgoto != null) {
-					httpServletRequest.setAttribute("comboLigacaoEsgoto", "sim");					if (indicadorRedeEsgoto.equalsIgnoreCase(""	+ Quadra.SEM_REDE)) {
-						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoEsgotoSituacao.ID, LigacaoEsgotoSituacao.POTENCIAL));						Collection<LigacaoEsgotoSituacao> colecaoLigacaoEsgotoSituacao = fachada								.pesquisar(filtroLigacaoEsgotoSituacao, LigacaoEsgotoSituacao.class.getName());
-						if (colecaoLigacaoEsgotoSituacao == null || colecaoLigacaoEsgotoSituacao.isEmpty()) {							throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",									null, "Ligacao Esgoto Situacao");						}
-						httpServletRequest.setAttribute("colecaoLigacaoEsgotoSituacao",	colecaoLigacaoEsgotoSituacao);					}
-					if (indicadorRedeEsgoto.equalsIgnoreCase("" + Quadra.COM_REDE)) {
-						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoEsgotoSituacao.ID,	LigacaoEsgotoSituacao.FACTIVEL));
+			if (form.getIndicadorTipoLigacao().equals("2") || form.getIndicadorTipoLigacao().equals("3")) {				if (indicadorRedeEsgoto != null) {
+					
+					Collection<LigacaoEsgotoSituacao> colecaoLigacaoEsgotoSituacao = null;
+					httpServletRequest.setAttribute("comboLigacaoEsgoto", "sim");
+										if (indicadorRedeEsgoto.equalsIgnoreCase(""	+ Quadra.SEM_REDE)) {
+						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoEsgotoSituacao.ID, LigacaoEsgotoSituacao.POTENCIAL));						colecaoLigacaoEsgotoSituacao = fachada.pesquisar(filtroLigacaoEsgotoSituacao, LigacaoEsgotoSituacao.class.getName());
+					}
+					if (indicadorRedeEsgoto.equalsIgnoreCase("" + Quadra.COM_REDE)) {						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoEsgotoSituacao.ID,	LigacaoEsgotoSituacao.FACTIVEL));
 						filtroLigacaoEsgotoSituacao.adicionarParametro( new ParametroSimples(
-								FiltroLigacaoEsgotoSituacao.INDICADORFATURAMENTOSITUACAO, ConstantesSistema.NAO ) );													Collection<LigacaoEsgotoSituacao> colecaoLigacaoEsgotoSituacao = fachada								.pesquisar(filtroLigacaoEsgotoSituacao, LigacaoEsgotoSituacao.class.getName());
-						if (colecaoLigacaoEsgotoSituacao == null || colecaoLigacaoEsgotoSituacao.isEmpty()) {							throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",									null, "Ligacao Esgoto Situacao");						}
-						httpServletRequest.setAttribute("colecaoLigacaoEsgotoSituacao",								colecaoLigacaoEsgotoSituacao);					}
-					if (indicadorRedeEsgoto.equalsIgnoreCase("" + Quadra.REDE_PARCIAL)) {
-						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(
+								FiltroLigacaoEsgotoSituacao.INDICADORFATURAMENTOSITUACAO, ConstantesSistema.NAO ) );													colecaoLigacaoEsgotoSituacao = fachada.pesquisar(filtroLigacaoEsgotoSituacao, LigacaoEsgotoSituacao.class.getName());					}
+					if (indicadorRedeEsgoto.equalsIgnoreCase("" + Quadra.REDE_PARCIAL)) {						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(
 								FiltroLigacaoEsgotoSituacao.ID, LigacaoEsgotoSituacao.POTENCIAL,								ConectorOr.CONECTOR_OR ));						filtroLigacaoEsgotoSituacao.adicionarParametro(new ParametroSimples(								FiltroLigacaoEsgotoSituacao.ID, LigacaoEsgotoSituacao.FACTIVEL,								ConectorAnd.CONECTOR_AND, 2));
 						filtroLigacaoEsgotoSituacao.adicionarParametro( new ParametroSimples(
 								FiltroLigacaoEsgotoSituacao.INDICADORFATURAMENTOSITUACAO,
-								ConstantesSistema.NAO));						Collection<LigacaoEsgotoSituacao> colecaoLigacaoEsgotoSituacao = fachada								.pesquisar(filtroLigacaoEsgotoSituacao, LigacaoEsgotoSituacao.class.getName());
-						if (colecaoLigacaoEsgotoSituacao == null || colecaoLigacaoEsgotoSituacao.isEmpty()) {							throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",									null, "Ligacao Esgoto Situacao");						}
-						httpServletRequest.setAttribute("colecaoLigacaoEsgotoSituacao", colecaoLigacaoEsgotoSituacao);					}				}			}		}	}}
+								ConstantesSistema.NAO));						colecaoLigacaoEsgotoSituacao = fachada.pesquisar(filtroLigacaoEsgotoSituacao, LigacaoEsgotoSituacao.class.getName());
+					}
+										if (colecaoLigacaoEsgotoSituacao == null || colecaoLigacaoEsgotoSituacao.isEmpty()) {						throw new ActionServletException("atencao.entidade_sem_dados_para_selecao",								null, "Ligacao Esgoto Situacao");					}					
+					httpServletRequest.setAttribute("colecaoLigacaoEsgotoSituacao", colecaoLigacaoEsgotoSituacao);				}			}		}	}}
