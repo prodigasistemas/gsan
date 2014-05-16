a3c0e8104a0bc8e560a72d46fc421dd6aa33465b
Pamela Gatinho <pamelagatinho@gmail.com>
2014-05-16 10:21:18 -0300

    Melhoria no código

--- src/gcom/micromedicao/medicao/MedicaoTipo.java
+++ src/gcom/micromedicao/medicao/MedicaoTipo.java
@@ -8,146 +8,62 @@ import java.util.Date;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-/**
- * @author Hibernate CodeGenerator
- */
 public class MedicaoTipo extends ObjetoTransacao {
 	private static final long serialVersionUID = 1L;
-    /**
-     * identifier field
-     */
-    private Integer id;
 
-    /**
-     * nullable persistent field
-     */
+	private Integer id;
     private String descricao;
-
-    /**
-     * nullable persistent field
-     */
     private Short indicadorUso;
-
-    /**
-     * nullable persistent field
-     */
     private Date ultimaAlteracao;
 
-    //--CONSTANTES
-    /**
-     * Description of the Field
-     */
     public final static Integer LIGACAO_AGUA = new Integer(1);
-
-    /**
-     * Description of the Field
-     */
     public final static Integer POCO = new Integer(2);
 
-    /**
-     * full constructor
-     * 
-     * @param descricao
-     *            Descrição do parâmetro
-     * @param indicadorUso
-     *            Descrição do parâmetro
-     * @param ultimaAlteracao
-     *            Descrição do parâmetro
-     */
-    public MedicaoTipo(String descricao, Short indicadorUso,
-            Date ultimaAlteracao) {
+    public MedicaoTipo(String descricao, Short indicadorUso, Date ultimaAlteracao) {
         this.descricao = descricao;
         this.indicadorUso = indicadorUso;
         this.ultimaAlteracao = ultimaAlteracao;
     }
 
-    /**
-     * default constructor
-     */
     public MedicaoTipo() {
     }
 
-    /**
-     * Retorna o valor de id
-     * 
-     * @return O valor de id
-     */
+    public MedicaoTipo(Integer id) {
+    	this.id = id;
+    }
+
     public Integer getId() {
         return this.id;
     }
 
-    /**
-     * Seta o valor de id
-     * 
-     * @param id
-     *            O novo valor de id
-     */
     public void setId(Integer id) {
         this.id = id;
     }
 
-    /**
-     * Retorna o valor de descricao
-     * 
-     * @return O valor de descricao
-     */
     public String getDescricao() {
         return this.descricao;
     }
 
-    /**
-     * Seta o valor de descricao
-     * 
-     * @param descricao
-     *            O novo valor de descricao
-     */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
-    /**
-     * Retorna o valor de indicadorUso
-     * 
-     * @return O valor de indicadorUso
-     */
     public Short getIndicadorUso() {
         return this.indicadorUso;
     }
 
-    /**
-     * Seta o valor de indicadorUso
-     * 
-     * @param indicadorUso
-     *            O novo valor de indicadorUso
-     */
     public void setIndicadorUso(Short indicadorUso) {
         this.indicadorUso = indicadorUso;
     }
 
-    /**
-     * Retorna o valor de ultimaAlteracao
-     * 
-     * @return O valor de ultimaAlteracao
-     */
     public Date getUltimaAlteracao() {
         return this.ultimaAlteracao;
     }
 
-    /**
-     * Seta o valor de ultimaAlteracao
-     * 
-     * @param ultimaAlteracao
-     *            O novo valor de ultimaAlteracao
-     */
     public void setUltimaAlteracao(Date ultimaAlteracao) {
         this.ultimaAlteracao = ultimaAlteracao;
     }
 
-    /**
-     * < <Descrição do método>>
-     * 
-     * @return Descrição do retorno
-     */
     public String toString() {
         return new ToStringBuilder(this).append("id", getId()).toString();
     }
