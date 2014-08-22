604eb7f0e1025358f3ecfa8fc51b1b52bd163ea3
Julian Fernando <julian.fernando@rps.com.br>
2014-08-22 10:09:47 -0300

    alterações

--- migrations/comercial/scripts/20140530203811_Add_colunas_tabelas_retorno_recadastramento.sql
+++ migrations/comercial/scripts/20140530203811_Add_colunas_tabelas_retorno_recadastramento.sql
@@ -1,17 +1,10 @@
 -- // Add colunas tabelas retorno recadastramento
 -- Migration SQL that makes the change goes here.
-
---ALTER TABLE atendimentopublico.ligacao_esgoto_situacao ADD PRIMARY KEY (lest_id);
---ALTER TABLE atendimentopublico.ramal_local_instalacao ADD PRIMARY KEY (rlin_id);
---ALTER TABLE micromedicao.hidrometro_marca  ADD PRIMARY KEY (himc_id);
---ALTER TABLE micromedicao.hidrometro_capacidade ADD PRIMARY KEY (hicp_id);
---ALTER TABLE cadastro.logradouro_tipo ADD PRIMARY KEY (lgtp_id);
-
 ALTER TABLE atualizacaocadastral.imovel_retorno ADD COLUMN lest_id INTEGER
 	CONSTRAINT fk4_imovel_retorno REFERENCES atendimentopublico.ligacao_esgoto_situacao (lest_id)
 	MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-	
+
 ALTER TABLE atualizacaocadastral.imovel_retorno ADD COLUMN rlin_id INTEGER
 	CONSTRAINT fk5_imovel_retorno REFERENCES atendimentopublico.ramal_local_instalacao (rlin_id)
 	MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT;
@@ -37,12 +30,7 @@ ALTER TABLE atualizacaocadastral.cliente_endereco_retorno ADD COLUMN lgtp_id INT
 -- //@UNDO
 -- SQL to undo the change goes here.
 ALTER TABLE atualizacaocadastral.imovel_retorno DROP COLUMN lest_id;
-ALTER TABLE atualizacaocadastral.imovel_retorno DROP CONSTRAINT fk4_imovel_retorno;
 ALTER TABLE atualizacaocadastral.imovel_retorno DROP COLUMN rlin_id;
-ALTER TABLE atualizacaocadastral.imovel_retorno DROP CONSTRAINT fk5_imovel_retorno;
 ALTER TABLE atualizacaocadastral.imovel_retorno DROP COLUMN himc_id;
-ALTER TABLE atualizacaocadastral.imovel_retorno DROP CONSTRAINT fk6_imovel_retorno;
 ALTER TABLE atualizacaocadastral.imovel_retorno DROP COLUMN hicp_id;
-ALTER TABLE atualizacaocadastral.imovel_retorno DROP CONSTRAINT fk7_imovel_retorno;
 ALTER TABLE atualizacaocadastral.cliente_endereco_retorno DROP COLUMN lgtp_id;
-ALTER TABLE atualizacaocadastral.cliente_endereco_retorno DROP CONSTRAINT fk3_cliente_endereco_retorno;
