f34805cc8f94b242d650e53e5957873c144c4db7
Pamela Gatinho <pamelagatinho@gmail.com>
2014-10-03 17:15:38 -0300

    Não permitir que pagamentos classificados pela recuperação de crédito sejam reclassificados. (#176)

--- src/gcom/arrecadacao/pagamento/Pagamento.java
+++ src/gcom/arrecadacao/pagamento/Pagamento.java
@@ -14,6 +14,7 @@ import gcom.faturamento.debito.DebitoACobrarGeral;
 import gcom.faturamento.debito.DebitoTipo;
 import gcom.interceptor.ControleAlteracao;
 import gcom.interceptor.ObjetoTransacao;
+import gcom.util.ConstantesSistema;
 import gcom.util.filtro.Filtro;
 import gcom.util.filtro.ParametroSimples;
 
@@ -22,8 +23,6 @@ import java.util.Date;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-/** @author Hibernate CodeGenerator */
-
 @ControleAlteracao()
 public class Pagamento extends ObjetoTransacao {
 
@@ -31,135 +30,83 @@ public class Pagamento extends ObjetoTransacao {
 	public static final int ATRIBUTOS_INSERIR_PAGAMENTO = 167; //Operacao.OPERACAO_INSERIR_PAGAMENTO
 	public static final int ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO = 1804; //Operacao.OPERACAO_INFORMAR_PAGAMENTO_CONTRATO_PARCELAMENTO_POR_CLIENTE;
 
-	/** identifier field */
 	private Integer id;
-
-	/** persistent field */
 	private int anoMesReferencia;
 
-	/** persistent field */
 	@ControleAlteracao(funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private Date dataPagamento;
 
-	/** nullable persistent field */
 	private Date ultimaAlteracao;
 
-	/** persistent field */
 	@ControleAlteracao(funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private int anoMesReferenciaArrecadacao;
 
-	/** persistent field */
-	@ControleAlteracao(value=FiltroPagamento.CONTA,
-			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO})
+	@ControleAlteracao(value=FiltroPagamento.CONTA, funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO})
 	private ContaGeral contaGeral;
 
-	/** persistent field */
-	@ControleAlteracao(value=FiltroPagamento.LOCALIDADE,
-			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
+	@ControleAlteracao(value=FiltroPagamento.LOCALIDADE, funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private Localidade localidade;
 
-	/** persistent field */
-	@ControleAlteracao(value=FiltroPagamento.DEBITO_TIPO_,
-			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
+	@ControleAlteracao(value=FiltroPagamento.DEBITO_TIPO_, funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private DebitoTipo debitoTipo;
 
-	/** persistent field */
-	@ControleAlteracao(value=FiltroPagamento.DEBITO_A_COBRAR_,
-			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO})
+	@ControleAlteracao(value=FiltroPagamento.DEBITO_A_COBRAR_, funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO})
 	private DebitoACobrarGeral debitoACobrarGeral;
 
-	/** persistent field */
 	private Short indicadorExpurgado;
+	private PagamentoSituacao pagamentoSituacaoAtual;
+	private PagamentoSituacao pagamentoSituacaoAnterior;
 
-	/** persistent field */
-	private gcom.arrecadacao.pagamento.PagamentoSituacao pagamentoSituacaoAtual;
-
-	/** persistent field */
-	private gcom.arrecadacao.pagamento.PagamentoSituacao pagamentoSituacaoAnterior;
-
-	/** persistent field */
 	@ControleAlteracao(value=FiltroPagamento.GUIA_PAGAMENTO,
 			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private gcom.arrecadacao.pagamento.GuiaPagamento guiaPagamento;
 
-	/** persistent field */
 	@ControleAlteracao(value=FiltroPagamento.DOCUMENTO_TIPO,
 			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private DocumentoTipo documentoTipo;
 
-	/** persistent field */
 	@ControleAlteracao(value=FiltroPagamento.AVISO_BANCARIO,
 			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private AvisoBancario avisoBancario;
 
-	/** persistent field */
 	@ControleAlteracao(value=FiltroPagamento.IMOVEL,
 			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private Imovel imovel;
 
-	/** persistent field */
 	private ArrecadadorMovimentoItem arrecadadorMovimentoItem;
 
-	/** persistent field */
 	@ControleAlteracao(value=FiltroPagamento.ARRECADACAO_FORMA,
 			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private ArrecadacaoForma arrecadacaoForma;
 
-	/** persistent field */
 	@ControleAlteracao(funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private BigDecimal valorPagamento;
 
-	/** persistent field */
 	private BigDecimal valorExcedente;
 
-	/** persistent field */
 	private Integer anoMesReferenciaPagamento;
 
-	/** persistent field */
 	@ControleAlteracao(value=FiltroPagamento.CLIENTE,
 			funcionalidade={ATRIBUTOS_INSERIR_PAGAMENTO, ATRIBUTOS_INSERIR_PAGAMENTO_CONTRATO_PARCELAMENTO})
 	private Cliente cliente;
 
 	private Integer quantidadePagamentos;
-
+	private Date dataPrevistaCreditoHelper;
+	private short indicadorPagamento;
+	private Date dataProcessamento;
+	private short indicadorClassificadoRecuperacaoCredito;
+	
 	private Fatura fatura;
-
 	private CobrancaDocumento cobrancaDocumento;
-	
 	private DocumentoTipo documentoTipoAgregador;
-	
 	private PagamentoCartaoDebito pagamentoCartaoDebito;
 
 	public final static Short INDICADOR_EXPURGADO_SIM = new Short("1");
-
 	public final static Short INDICADOR_EXPURGADO_NAO = new Short("2");
 
-	/**
-	 * [UC0242] - Registrar Movimento dos Arrecadadores [SF0004] - Processar
-	 * Registro Código F Autor: Sávio Luiz Data: 15/02/2006
-	 */
-
-	private Date dataPrevistaCreditoHelper;
-
-	/**
-	 * [UC0739] Informar Situação de Expurgadodo Pagamento Indicador que dirá se
-	 * é de pagamento ou pagamento_historico,foi utilizado esse indicador porque
-	 * em pagamento e pagamento_historico pode ter o mesmo id.
-	 * 
-	 * @author Sávio Luiz
-	 * @date 18/02/2008
-	 * @return void
-	 */
-
-	private short indicadorPagamento;
-
-	private Date dataProcessamento;
-
 	public final static Short INDICADOR_PAGAMENTO_SIM = new Short("1");
-
 	public final static Short INDICADOR_PAGAMENTO_NAO = new Short("2");
 
-	/** full constructor */
 	public Pagamento(
 			int anoMesReferencia,
 			Date dataPagamento,
@@ -193,13 +140,13 @@ public class Pagamento extends ObjetoTransacao {
 		this.valorPagamento = valorPagamento;
 		this.anoMesReferenciaPagamento = anoMesReferenciaPagamento;
 		this.cliente = cliente;
+		this.indicadorClassificadoRecuperacaoCredito = ConstantesSistema.NAO;
 	}
 
-	/** default constructor */
 	public Pagamento() {
+		this.indicadorClassificadoRecuperacaoCredito = ConstantesSistema.NAO;
 	}
 
-	/** minimal constructor */
 	public Pagamento(
 			int anoMesReferencia,
 			Date dataPagamento,
@@ -231,12 +178,9 @@ public class Pagamento extends ObjetoTransacao {
 		this.valorPagamento = valorPagamento;
 		this.anoMesReferenciaPagamento = anoMesReferenciaPagamento;
 		this.cliente = cliente;
+		this.indicadorClassificadoRecuperacaoCredito = ConstantesSistema.NAO;
 	}
 
-	/**
-	 * constructor para o caso de uso [UC0301] Gerar Dados Diários da
-	 * Arrecadação
-	 */
 	public Pagamento(Imovel imovel, BigDecimal valorPagamento,
 			Integer quantidadePagamentos, AvisoBancario avisoBancario,
 			Localidade localidade, DocumentoTipo documentoTipo,
@@ -250,6 +194,7 @@ public class Pagamento extends ObjetoTransacao {
 		this.documentoTipo = documentoTipo;
 		this.arrecadacaoForma = arrecadacaoForma;
 		this.dataPagamento = dataPagamento;
+		this.indicadorClassificadoRecuperacaoCredito = ConstantesSistema.NAO;
 	}
 
 	public Integer getId() {
@@ -388,9 +333,6 @@ public class Pagamento extends ObjetoTransacao {
 		return new ToStringBuilder(this).append("id", getId()).toString();
 	}
 
-	/**
-	 * @return Returns the valorPagamento.
-	 */
 	public BigDecimal getValorPagamento() {
 		if(valorPagamento != null){
 			return valorPagamento.setScale(2);
@@ -398,40 +340,23 @@ public class Pagamento extends ObjetoTransacao {
 			return valorPagamento;	
 		}
 	}
-	/**
-	 * @param valorPagamento
-	 *            The valorPagamento to set.
-	 */
+
 	public void setValorPagamento(BigDecimal valorPagamento) {
 		this.valorPagamento = valorPagamento;
 	}
 
-	/**
-	 * @return Returns the anoMesReferenciaPagamento.
-	 */
 	public Integer getAnoMesReferenciaPagamento() {
 		return anoMesReferenciaPagamento;
 	}
 
-	/**
-	 * @param anoMesReferenciaPagamento
-	 *            The anoMesReferenciaPagamento to set.
-	 */
 	public void setAnoMesReferenciaPagamento(Integer anoMesReferenciaPagamento) {
 		this.anoMesReferenciaPagamento = anoMesReferenciaPagamento;
 	}
 
-	/**
-	 * @return Returns the cliente.
-	 */
 	public Cliente getCliente() {
 		return cliente;
 	}
 
-	/**
-	 * @param cliente
-	 *            The cliente to set.
-	 */
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
@@ -512,24 +437,16 @@ public class Pagamento extends ObjetoTransacao {
 		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("contaGeral");
 		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("localidade");
 		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("debitoTipo");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("debitoACobrarGeral");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("pagamentoSituacaoAtual");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("guiaPagamento");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("documentoTipo");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("avisoBancario");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("debitoACobrarGeral");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("pagamentoSituacaoAtual");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("guiaPagamento");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("documentoTipo");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("avisoBancario");
 		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("imovel");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("arrecadadorMovimentoItem");
-		filtroPagamento
-				.adicionarCaminhoParaCarregamentoEntidade("arrecadacaoForma");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("arrecadadorMovimentoItem");
+		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("arrecadacaoForma");
 		filtroPagamento.adicionarCaminhoParaCarregamentoEntidade("cliente");
-		filtroPagamento.adicionarParametro(new ParametroSimples(
-				FiltroPagamento.ID, this.getId()));
+		filtroPagamento.adicionarParametro(new ParametroSimples(FiltroPagamento.ID, this.getId()));
 		return filtroPagamento;
 	}
 
@@ -608,4 +525,13 @@ public class Pagamento extends ObjetoTransacao {
 
 		return anoMesFormatado.toString();
 	}
+
+	public short getIndicadorClassificadoRecuperacaoCredito() {
+		return indicadorClassificadoRecuperacaoCredito;
+	}
+
+	public void setIndicadorClassificadoRecuperacaoCredito(short indicadorClassificadoRecuperacaoCredito) {
+		this.indicadorClassificadoRecuperacaoCredito = indicadorClassificadoRecuperacaoCredito;
+	}
+	
 }
