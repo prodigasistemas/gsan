f0a2d610796e7eb693e55da6bafbd0a3ff0b2137
Felipe <felipesantos2089@gmail.com>
2015-06-02 16:49:34 -0300

    Criando o DTO do relatório e adicionando o jar do cliente

---
+++ src/gcom/relatorio/arrecadacao/resumocreditosavisosbancarios/ResumoCreditosAvisosBancariosDTO.java
@@ -0,0 +1,10 @@
+package gcom.relatorio.arrecadacao.resumocreditosavisosbancarios;
+
+import br.com.prodigasistemas.gsan.relatorio.ReportItemDTO;
+
+public class ResumoCreditosAvisosBancariosDTO implements ReportItemDTO {
+
+	private static final long serialVersionUID = 7599238519918851586L;
+	
+	
+}
