77dd14d1eef24f3bf26b6c33d8e8d3a91791c46b
Paulo Igor <piagodinho@gmail.com>
2016-02-02 18:31:43 -0300

    adicionando novos testes para os métodos de cálculo da classe Conta

--- test/gcom/faturamento/TesteConta.java
+++ test/gcom/faturamento/TesteConta.java
@@ -1,23 +1,43 @@
 package gcom.faturamento;
 
+import static org.junit.Assert.assertEquals;
+
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import gcom.faturamento.conta.Conta;
 
 public class TesteConta {
 
-	@Test
-	public void testValorTotalContaSemImpostos() {
-		Conta conta = new Conta();
+	private Conta conta;
+	
+	@Before
+	public void setup() {
+		conta = new Conta();
 		conta.setValorAgua(new BigDecimal(10.2));
 		conta.setValorEsgoto(new BigDecimal(10.3));
 		conta.setDebitos(new BigDecimal(10.4));
 		conta.setValorCreditos(new BigDecimal(10.5));
-		
+		conta.setValorImposto(new BigDecimal(10.0));
+		conta.setValorRateioAgua(new BigDecimal(10.0));
+		conta.setValorRateioEsgoto(new BigDecimal(10.0));
+	}
+	
+	@Test
+	public void testValorTotalContaSemImpostos() {
 		assertEquals(20.40, conta.getValorTotalContaSemImposto().doubleValue(), 0.1);		
+	}
+	
+	@Test
+	public void testValorTotalContaComRateioBigDecimal() {
+		assertEquals(30.40, conta.getValorTotalContaComRateioBigDecimal().doubleValue(), 0.1);
+	}
+	
+	@Test
+	public void testValorTotalContaBigDecimal() {
+		assertEquals(10.40, conta.getValorTotalContaBigDecimal().doubleValue(), 0.1);
 	}
 
 }
