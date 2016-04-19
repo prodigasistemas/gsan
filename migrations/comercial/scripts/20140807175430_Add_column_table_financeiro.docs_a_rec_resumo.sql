4af9f40f4d774fc434f98bfad81cc20fdadae3b7
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2016-04-19 10:49:52 -0300

    Exclusao de arquivos do mybatis porque passamos a usar gsan-migracoes

--- migrations/comercial/scripts/20140807175430_Add_column_table_financeiro.docs_a_rec_resumo.sql
+++
@@ -1,9 +0,0 @@
--- // Add column table financeiro.docs_a_rec_resumo
--- Migration SQL that makes the change goes here.
-
-ALTER TABLE financeiro.docs_a_rec_resumo add column drrs_vldocumentossemparcelaatual numeric(13,2)
-
--- //@UNDO
--- SQL to undo the change goes here.
-
-ALTER TABLE financeiro.docs_a_rec_resumo drop column drrs_vldocumentossemparcelaatual
