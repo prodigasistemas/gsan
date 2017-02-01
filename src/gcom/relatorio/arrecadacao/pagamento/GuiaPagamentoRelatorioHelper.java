7d5ae127e64250efe4c7e778bbad27c3d4782e5c
Felipe <felipesantos2089@gmail.com>
2017-02-01 17:42:50 -0300

    Removendo formatação da matrícula no documento de Guia de Pagamento

--- src/gcom/relatorio/arrecadacao/pagamento/GuiaPagamentoRelatorioHelper.java
+++ src/gcom/relatorio/arrecadacao/pagamento/GuiaPagamentoRelatorioHelper.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class GuiaPagamentoRelatorioHelper {
-	
+
 	private Integer idImovel;
 	private Integer idCliente;
 	private Date dataVencimento;
@@ -16,141 +16,122 @@ public class GuiaPagamentoRelatorioHelper {
 	private String descLocalidade;
 	private BigDecimal valorDebito;
 	private Date dataEmissao;
-	private String nomeCliente; 
+	private String nomeCliente;
 	private Integer idTipoDebito;
 	private String descTipoDebito;
-    private Short numeroPrestacaoDebito;
-    private Short numeroPrestacaoTotal;
-    private String observacao;
-    private Short indicadorEmitirObservacao;
-	
+	private Short numeroPrestacaoDebito;
+	private Short numeroPrestacaoTotal;
+	private String observacao;
+	private Short indicadorEmitirObservacao;
+
 	private String matricula;
-	private String inscricao; 
-	private String enderecoImovel; 
+	private String inscricao;
+	private String enderecoImovel;
 	private String enderecoClienteResponsavel;
 	private String representacaoNumericaCodBarraFormatada;
 	private String representacaoNumericaCodBarraSemDigito;
 	private String idGuiaPagamento;
-	
+
 	private String cpfCliente;
 	private String cnpjCliente;
-	
-	
-	/* Ficha de Compensação
-	 * Adicionado por: Mariana Victor
-	 * Data: 02/03/2011
-	 * */
+
 	private String nossoNumero;
 	private String sacadoParte01;
 	private String sacadoParte02;
 	private String enderecoImovelSacado;
-	
+
 	private String nomeImovel;
 	private String subRelatorio;
-	
-	/** minimal constructor */
-    public GuiaPagamentoRelatorioHelper(
-    		Integer idImovel,
-    		Date dataVencimento,
-    		Integer idLocalidade,
-    		String descLocalidade,
-    		BigDecimal valorDebito,
-    		Date dataEmissao,
-    		Integer idTipoDebito,
-    		String descTipoDebito,
-            Short numeroPrestacaoDebito,
-            Short numeroPrestacaoTotal,
-            String observacao,
-            Short indicadorEmitirObservacao) {
-        this.idImovel = idImovel;
-        this.dataVencimento = dataVencimento;
-        this.idLocalidade = idLocalidade;
-        this.descLocalidade = descLocalidade;
-        this.valorDebito = valorDebito;
-        this.dataEmissao = dataEmissao;
-        this.idTipoDebito = idTipoDebito;
-        this.descTipoDebito = descTipoDebito;
-        this.numeroPrestacaoDebito = numeroPrestacaoDebito;
-        this.numeroPrestacaoTotal = numeroPrestacaoTotal;
-        this.observacao = observacao;
-        this.indicadorEmitirObservacao = indicadorEmitirObservacao;
-        
-    }
-    
-    public GuiaPagamentoRelatorioHelper(
-    		Integer idImovel,
-    		Date dataVencimento,
-    		Integer idLocalidade,
-    		String descLocalidade,
-    		BigDecimal valorDebito,
-    		Date dataEmissao,
-    		Integer idTipoDebito,
-    		String descTipoDebito,
-            Short numeroPrestacaoDebito,
-            Short numeroPrestacaoTotal,
-            String observacao,
-            Short indicadorEmitirObservacao,
-            String nomeImovel) {
-        this.idImovel = idImovel;
-        this.dataVencimento = dataVencimento;
-        this.idLocalidade = idLocalidade;
-        this.descLocalidade = descLocalidade;
-        this.valorDebito = valorDebito;
-        this.dataEmissao = dataEmissao;
-        this.idTipoDebito = idTipoDebito;
-        this.descTipoDebito = descTipoDebito;
-        this.numeroPrestacaoDebito = numeroPrestacaoDebito;
-        this.numeroPrestacaoTotal = numeroPrestacaoTotal;
-        this.observacao = observacao;
-        this.indicadorEmitirObservacao = indicadorEmitirObservacao;
-        this.nomeImovel = nomeImovel;
-        
-    }
-    public GuiaPagamentoRelatorioHelper() {
-    }
+
+	public GuiaPagamentoRelatorioHelper(Integer idImovel, Date dataVencimento, Integer idLocalidade, String descLocalidade, BigDecimal valorDebito, Date dataEmissao, Integer idTipoDebito,
+			String descTipoDebito, Short numeroPrestacaoDebito, Short numeroPrestacaoTotal, String observacao, Short indicadorEmitirObservacao) {
+		this.idImovel = idImovel;
+		this.dataVencimento = dataVencimento;
+		this.idLocalidade = idLocalidade;
+		this.descLocalidade = descLocalidade;
+		this.valorDebito = valorDebito;
+		this.dataEmissao = dataEmissao;
+		this.idTipoDebito = idTipoDebito;
+		this.descTipoDebito = descTipoDebito;
+		this.numeroPrestacaoDebito = numeroPrestacaoDebito;
+		this.numeroPrestacaoTotal = numeroPrestacaoTotal;
+		this.observacao = observacao;
+		this.indicadorEmitirObservacao = indicadorEmitirObservacao;
+
+	}
+
+	public GuiaPagamentoRelatorioHelper(Integer idImovel, Date dataVencimento, Integer idLocalidade, String descLocalidade, BigDecimal valorDebito, Date dataEmissao, Integer idTipoDebito,
+			String descTipoDebito, Short numeroPrestacaoDebito, Short numeroPrestacaoTotal, String observacao, Short indicadorEmitirObservacao, String nomeImovel) {
+		this.idImovel = idImovel;
+		this.dataVencimento = dataVencimento;
+		this.idLocalidade = idLocalidade;
+		this.descLocalidade = descLocalidade;
+		this.valorDebito = valorDebito;
+		this.dataEmissao = dataEmissao;
+		this.idTipoDebito = idTipoDebito;
+		this.descTipoDebito = descTipoDebito;
+		this.numeroPrestacaoDebito = numeroPrestacaoDebito;
+		this.numeroPrestacaoTotal = numeroPrestacaoTotal;
+		this.observacao = observacao;
+		this.indicadorEmitirObservacao = indicadorEmitirObservacao;
+		this.nomeImovel = nomeImovel;
+	}
+
+	public GuiaPagamentoRelatorioHelper() {
+	}
 
 	public String getDescTipoDebito() {
 		return descTipoDebito;
 	}
+
 	public void setDescTipoDebito(String descTipoDebito) {
 		this.descTipoDebito = descTipoDebito;
 	}
+
 	public String getRepresentacaoNumericaCodBarraFormatada() {
 		return representacaoNumericaCodBarraFormatada;
 	}
-	public void setRepresentacaoNumericaCodBarraFormatada(
-			String representacaoNumericaCodBarraFormatada) {
+
+	public void setRepresentacaoNumericaCodBarraFormatada(String representacaoNumericaCodBarraFormatada) {
 		this.representacaoNumericaCodBarraFormatada = representacaoNumericaCodBarraFormatada;
 	}
+
 	public String getRepresentacaoNumericaCodBarraSemDigito() {
 		return representacaoNumericaCodBarraSemDigito;
 	}
-	public void setRepresentacaoNumericaCodBarraSemDigito(
-			String representacaoNumericaCodBarraSemDigito) {
+
+	public void setRepresentacaoNumericaCodBarraSemDigito(String representacaoNumericaCodBarraSemDigito) {
 		this.representacaoNumericaCodBarraSemDigito = representacaoNumericaCodBarraSemDigito;
 	}
 
 	public Date getDataEmissao() {
 		return dataEmissao;
 	}
+
 	public void setDataEmissao(Date dataEmissao) {
 		this.dataEmissao = dataEmissao;
 	}
+
 	public Integer getIdCliente() {
 		return idCliente;
 	}
+
 	public void setIdCliente(Integer idCliente) {
 		this.idCliente = idCliente;
 	}
+
 	public Integer getIdImovel() {
 		return idImovel;
 	}
+
 	public void setIdImovel(Integer idImovel) {
 		this.idImovel = idImovel;
 	}
+
 	public Integer getIdLocalidade() {
 		return idLocalidade;
 	}
+
 	public void setIdLocalidade(Integer idLocalidade) {
 		this.idLocalidade = idLocalidade;
 	}
@@ -159,27 +140,30 @@ public class GuiaPagamentoRelatorioHelper {
 		return valorDebito;
 	}
 
-
 	public void setValorDebito(BigDecimal valorDebito) {
 		this.valorDebito = valorDebito;
 	}
 
-
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
+
 	public void setDataVencimento(Date dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
+
 	public String getEnderecoClienteResponsavel() {
 		return enderecoClienteResponsavel;
 	}
+
 	public void setEnderecoClienteResponsavel(String enderecoClienteResponsavel) {
 		this.enderecoClienteResponsavel = enderecoClienteResponsavel;
 	}
+
 	public String getEnderecoImovel() {
 		return enderecoImovel;
 	}
+
 	public void setEnderecoImovel(String enderecoImovel) {
 		this.enderecoImovel = enderecoImovel;
 	}
@@ -187,22 +171,27 @@ public class GuiaPagamentoRelatorioHelper {
 	public String getInscricao() {
 		return inscricao;
 	}
+
 	public void setInscricao(String inscricao) {
 		this.inscricao = inscricao;
 	}
+
 	public String getMatricula() {
 		return matricula;
 	}
+
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
+
 	public String getNomeCliente() {
 		return nomeCliente;
 	}
+
 	public void setNomeCliente(String nomeCliente) {
 		this.nomeCliente = nomeCliente;
 	}
-	
+
 	public String getDescLocalidade() {
 		return descLocalidade;
 	}
@@ -227,116 +216,78 @@ public class GuiaPagamentoRelatorioHelper {
 		this.idGuiaPagamento = idGuiaPagamento;
 	}
 
-	//Data de Validade da Guia de Pagamento
-	//último dia do proximo mês do mês/ano da data de vencimento da guia de pagamento
-	public String getDataValidade(){
-		
-		int anoVencimento = Util.getAno(getDataVencimento()); 
-		int mesVencimento = Util.getMes(getDataVencimento()); 
-		
-		Calendar calendarUltimoDiaMesAnoDataVencimento  = new GregorianCalendar();
-		
+	// Data de Validade da Guia de Pagamento último dia do proximo mês do mês/ano da data de vencimento da guia de pagamento
+	public String getDataValidade() {
+		int anoVencimento = Util.getAno(getDataVencimento());
+		int mesVencimento = Util.getMes(getDataVencimento());
+
+		Calendar calendarUltimoDiaMesAnoDataVencimento = new GregorianCalendar();
+
 		calendarUltimoDiaMesAnoDataVencimento.set(Calendar.YEAR, anoVencimento);
 		calendarUltimoDiaMesAnoDataVencimento.set(Calendar.MONTH, mesVencimento - 1);
-		calendarUltimoDiaMesAnoDataVencimento.set(Calendar.DAY_OF_MONTH,
-				calendarUltimoDiaMesAnoDataVencimento.getActualMaximum(Calendar.DAY_OF_MONTH));
-		
-		Date dateDataVencimentoMais3Dias = Util.adicionarNumeroDiasDeUmaData(getDataVencimento(),3);
-		Date dateDataCorrenteMais3Dias = Util.adicionarNumeroDiasDeUmaData(new Date(),3);
+		calendarUltimoDiaMesAnoDataVencimento.set(Calendar.DAY_OF_MONTH, calendarUltimoDiaMesAnoDataVencimento.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+		Date dateDataVencimentoMais3Dias = Util.adicionarNumeroDiasDeUmaData(getDataVencimento(), 3);
+		Date dateDataCorrenteMais3Dias = Util.adicionarNumeroDiasDeUmaData(new Date(), 3);
 		Date dateMaiorData = null;
-		
-//		 retorna 
-//		 -1 se a data1 for menor que a data2, 
-//		 0 se as datas forem iguais,
-//		 1 se a data1 for maior que a data2.
-		
-		if (Util.compararData(dateDataVencimentoMais3Dias,dateDataCorrenteMais3Dias) >= 0){
-			
-			if (Util.compararData(dateDataVencimentoMais3Dias,calendarUltimoDiaMesAnoDataVencimento.getTime()) >= 0){
+
+		if (Util.compararData(dateDataVencimentoMais3Dias, dateDataCorrenteMais3Dias) >= 0) {
+
+			if (Util.compararData(dateDataVencimentoMais3Dias, calendarUltimoDiaMesAnoDataVencimento.getTime()) >= 0) {
 				dateMaiorData = dateDataVencimentoMais3Dias;
-			}else{
+			} else {
 				dateMaiorData = calendarUltimoDiaMesAnoDataVencimento.getTime();
 			}
-			
-		}else{
-			
-			if (Util.compararData(dateDataCorrenteMais3Dias,calendarUltimoDiaMesAnoDataVencimento.getTime()) >= 0){
+
+		} else {
+			if (Util.compararData(dateDataCorrenteMais3Dias, calendarUltimoDiaMesAnoDataVencimento.getTime()) >= 0) {
 				dateMaiorData = dateDataCorrenteMais3Dias;
-			}else{
+			} else {
 				dateMaiorData = calendarUltimoDiaMesAnoDataVencimento.getTime();
 			}
 		}
-		
-		
-//		String anoMesValidade = Util.getAnoMesComoString(getDataVencimento());
-//		Calendar calendario = new GregorianCalendar();
-//
-//		calendario.set(Calendar.YEAR, new Integer(anoMesValidade.substring(0, 4)).intValue());
-//		calendario.set(Calendar.MONTH, new Integer(anoMesValidade.substring(4, 6)).intValue());
-//		calendario.set(Calendar.DAY_OF_MONTH, calendario.getActualMaximum(Calendar.DAY_OF_MONTH));
-		
-		
+
 		return Util.formatarData(dateMaiorData);
 	}
-	
-	public String getMatriculaFormatada(){
-		String matriculaFormatada = Util.adicionarZerosEsquedaNumero(9, getMatricula());
-		matriculaFormatada = matriculaFormatada.substring(0, 8)
-				+ "." + matriculaFormatada.substring(8, 9);
-		return  matriculaFormatada ;
+
+	public Short getNumeroPrestacaoDebito() {
+		return numeroPrestacaoDebito;
 	}
 
-    public Short getNumeroPrestacaoDebito() {
-        return numeroPrestacaoDebito;
-    }
+	public void setNumeroPrestacaoDebito(Short numeroPrestacaoDebito) {
+		this.numeroPrestacaoDebito = numeroPrestacaoDebito;
+	}
 
-    public void setNumeroPrestacaoDebito(Short numeroPrestacaoDebito) {
-        this.numeroPrestacaoDebito = numeroPrestacaoDebito;
-    }
+	public Short getNumeroPrestacaoTotal() {
+		return numeroPrestacaoTotal;
+	}
 
-    public Short getNumeroPrestacaoTotal() {
-        return numeroPrestacaoTotal;
-    }
+	public void setNumeroPrestacaoTotal(Short numeroPrestacaoTotal) {
+		this.numeroPrestacaoTotal = numeroPrestacaoTotal;
+	}
 
-    public void setNumeroPrestacaoTotal(Short numeroPrestacaoTotal) {
-        this.numeroPrestacaoTotal = numeroPrestacaoTotal;
-    }
+	public String getPrestacaoFormatada() {
+		String prestacaoFormatada = "";
 
-    public String getPrestacaoFormatada(){
-        String prestacaoFormatada = "";
-        
-        if(getNumeroPrestacaoDebito() != null &&
-                getNumeroPrestacaoTotal() != null){
-            prestacaoFormatada = prestacaoFormatada + getNumeroPrestacaoDebito() + "/" + getNumeroPrestacaoTotal();
-        }
-        
-        return  prestacaoFormatada ;
-    }
+		if (getNumeroPrestacaoDebito() != null && getNumeroPrestacaoTotal() != null) {
+			prestacaoFormatada = prestacaoFormatada + getNumeroPrestacaoDebito() + "/" + getNumeroPrestacaoTotal();
+		}
 
-	/**
-	 * @return Retorna o campo observacao.
-	 */
+		return prestacaoFormatada;
+	}
+
 	public String getObservacao() {
 		return observacao;
 	}
 
-	/**
-	 * @param observacao O observacao a ser setado.
-	 */
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
 
-	/**
-	 * @return Retorna o campo indicadorEmitirObservacao.
-	 */
 	public Short getIndicadorEmitirObservacao() {
 		return indicadorEmitirObservacao;
 	}
 
-	/**
-	 * @param indicadorEmitirObservacao O indicadorEmitirObservacao a ser setado.
-	 */
 	public void setIndicadorEmitirObservacao(Short indicadorEmitirObservacao) {
 		this.indicadorEmitirObservacao = indicadorEmitirObservacao;
 	}
@@ -404,5 +355,4 @@ public class GuiaPagamentoRelatorioHelper {
 	public void setSubRelatorio(String subRelatorio) {
 		this.subRelatorio = subRelatorio;
 	}
-    
 }
