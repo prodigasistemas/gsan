2be207b73bfdd8c40e363af6a14c789d286dc5e3
Matheus Cruz <matheus1souza27@gmail.com>
2017-06-07 16:27:22 -0300

    Adicao de demonstrativo de impostos na geracao de arquivos para Printer

--- src/gcom/faturamento/ControladorFaturamentoCOSANPASEJB.java
+++ src/gcom/faturamento/ControladorFaturamentoCOSANPASEJB.java
@@ -515,6 +515,9 @@ public class ControladorFaturamentoCOSANPASEJB extends ControladorFaturamento
 
 								contaTxt = preencherDadosRateioAguaEsgoto(emitirContaHelper, contaTxt);
 								
+								contaTxt = preencherDadosAliquotaImposto(emitirContaHelper, contaTxt);
+								
+								
 								if (imovelEmitido.getQuadra().getRota().getIndicadorImpressaoTermicaFinalGrupo().equals(ConstantesSistema.SIM)
 										&& municipioEntrega.equals(municipioImovel)) {
 
@@ -3507,5 +3510,24 @@ public class ControladorFaturamentoCOSANPASEJB extends ControladorFaturamento
 			colecaoDebitosACobrarCategorias.addAll((Collection<DebitoACobrarCategoria>)inserirDebitoACobrarCategoriaBatch(debito, debito.getImovel()));
 		}
 		return colecaoDebitosACobrarCategorias;
+	}
+
+	public StringBuilder preencherDadosAliquotaImposto(EmitirContaHelper emitirContaHelper, StringBuilder contaTxt) throws ControladorException {
+		Object[] dadosAliquotasImpostos = gerarDadosAliquotasImpostos(emitirContaHelper, false);
+		
+		if (dadosAliquotasImpostos.length <= 0) {
+			return null;
+		}
+		
+		contaTxt.append(Util.completaString("Tributos", 30));
+		contaTxt.append(Util.completaString((String) dadosAliquotasImpostos[0], 30));
+		contaTxt.append(Util.completaString("(%)", 30));
+		contaTxt.append(Util.completaStringComEspacoAEsquerda(Util.formatarMoedaReal((BigDecimal) dadosAliquotasImpostos[1]), 13));
+		contaTxt.append(Util.completaString("Base de cálculo", 30));
+		contaTxt.append(Util.completaStringComEspacoAEsquerda(Util.formatarMoedaReal((BigDecimal) dadosAliquotasImpostos[2]), 13));
+		contaTxt.append(Util.completaString("Valor (R$)", 30));
+		contaTxt.append(Util.completaStringComEspacoAEsquerda(Util.formatarMoedaReal((BigDecimal) dadosAliquotasImpostos[3]), 13));
+		
+		return contaTxt;
 	}
 }
