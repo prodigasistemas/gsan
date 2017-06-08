f5f4e67f778744dd5335c4867494950105936374
Matheus Cruz <matheus1souza27@gmail.com>
2017-06-08 18:14:43 -0300

    Alterar para cancelado os debitos a cobrar de parcelamentos cancelados

--- src/gcom/cobranca/controladores/ControladorParcelamento.java
+++ src/gcom/cobranca/controladores/ControladorParcelamento.java
@@ -2,11 +2,18 @@ package gcom.cobranca.controladores;
 
 import gcom.cobranca.repositorios.IRepositorioParcelamentoHBM;
 import gcom.cobranca.repositorios.RepositorioParcelamentoHBM;
+import gcom.faturamento.debito.DebitoACobrar;
+import gcom.faturamento.debito.DebitoCreditoSituacao;
+import gcom.faturamento.debito.FiltroDebitoACobrar;
+import gcom.faturamento.debito.FiltroDebitoCreditoSituacao;
 import gcom.util.ControladorComum;
 import gcom.util.ControladorException;
 import gcom.util.ErroRepositorioException;
+import gcom.util.filtro.ParametroSimples;
 
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.CreateException;
@@ -16,6 +23,8 @@ public class ControladorParcelamento extends ControladorComum {
 	private static final long serialVersionUID = -2063305788601928963L;
 
 	protected IRepositorioParcelamentoHBM repositorioParcelamento;
+	
+	private DebitoCreditoSituacao situacaoCancelada;
 
 	public void ejbCreate() throws CreateException {
 		repositorioParcelamento = RepositorioParcelamentoHBM.getInstancia();
@@ -24,6 +33,9 @@ public class ControladorParcelamento extends ControladorComum {
 	public void cancelarParcelamentos() throws ControladorException {
 		try {
 			List<Object[]> parcelamentos = repositorioParcelamento.pesquisarParcelamentosParaCancelar();
+			FiltroDebitoCreditoSituacao filtroDebitoCreditoSituacao = new  FiltroDebitoCreditoSituacao();
+			filtroDebitoCreditoSituacao.adicionarParametro(new ParametroSimples(FiltroDebitoCreditoSituacao.ID, DebitoCreditoSituacao.CANCELADA));
+			situacaoCancelada = (DebitoCreditoSituacao) super.getControladorUtil().pesquisar(filtroDebitoCreditoSituacao, DebitoCreditoSituacao.class.toString()).iterator().next();
 			
 			for (Object[] parcelamento : parcelamentos) {
 				Integer idParcelamento = (Integer) parcelamento[0];
@@ -44,7 +56,24 @@ public class ControladorParcelamento extends ControladorComum {
 	}
 
 	private void cancelarDebitoACobrar(Integer idParcelamento) {
-
+		try {
+			FiltroDebitoACobrar filtroDebitoACobrar = new FiltroDebitoACobrar();
+			filtroDebitoACobrar.adicionarParametro(new ParametroSimples(FiltroDebitoACobrar.PARCELAMENTO_ID, idParcelamento));
+			Collection<DebitoACobrar> colecaoDebitoACobrar = super.getControladorUtil().pesquisar(filtroDebitoACobrar, DebitoACobrar.class.toString());
+			
+			if (colecaoDebitoACobrar != null && !colecaoDebitoACobrar.isEmpty()) {
+				for (DebitoACobrar debitoACobrar : colecaoDebitoACobrar) {
+					debitoACobrar.setDebitoCreditoSituacaoAnterior(debitoACobrar.getDebitoCreditoSituacaoAtual());
+					debitoACobrar.setDebitoCreditoSituacaoAtual(situacaoCancelada);
+					debitoACobrar.setUltimaAlteracao(new Date());
+					
+					super.getControladorUtil().atualizar(debitoACobrar);
+				}
+			}
+			
+		} catch (ControladorException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void cancelarCreditoARealizar(Integer idParcelamento) {
