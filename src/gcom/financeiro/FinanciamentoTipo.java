b0a17ba1320709e20334a9b111dfde20bb20b775
Felipe <felipesantos2089@gmail.com>
2017-06-16 14:30:20 -0300

    Finalizando método para gerar débito a cobrar de parcelamento cancelado

--- src/gcom/financeiro/FinanciamentoTipo.java
+++ src/gcom/financeiro/FinanciamentoTipo.java
@@ -8,103 +8,99 @@ import java.util.Date;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-
-/** @author Hibernate CodeGenerator */
 public class FinanciamentoTipo extends ObjetoTransacao {
+
 	private static final long serialVersionUID = 1L;
-    //Constantes tipo de financiamento >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
-    public final static Integer SERVICO_NORMAL = new Integer(1);
-    public final static Integer PARCELAMENTO_AGUA = new Integer(2);
-    public final static Integer PARCELAMENTO_ESGOTO = new Integer(3);
-    public final static Integer PARCELAMENTO_SERVICO = new Integer(4);
-    public final static Integer ARRASTO_AGUA = new Integer(5);
-    public final static Integer ARRASTO_ESGOTO = new Integer(6);
-    public final static Integer ARRASTO_SERVICO = new Integer(7);
-    public final static Integer JUROS_PARCELAMENTO = new Integer(8);
-    public final static Integer ENTRADA_PARCELAMENTO = new Integer(9);
-    public final static Integer DOACOES = new Integer(10);
-    
-    public final static Short INDICADOR_INCLUSAO_SIM = new Short("1");
-    public final static Short INDICADOR_INCLUSAO_NAO = new Short("2");
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>	
-    
-	
-    /** identifier field */
-    private Integer id;
 
-    /** nullable persistent field */
-    private String descricao;
+	public final static Integer SERVICO_NORMAL = new Integer(1);
+	public final static Integer PARCELAMENTO_AGUA = new Integer(2);
+	public final static Integer PARCELAMENTO_ESGOTO = new Integer(3);
+	public final static Integer PARCELAMENTO_SERVICO = new Integer(4);
+	public final static Integer ARRASTO_AGUA = new Integer(5);
+	public final static Integer ARRASTO_ESGOTO = new Integer(6);
+	public final static Integer ARRASTO_SERVICO = new Integer(7);
+	public final static Integer JUROS_PARCELAMENTO = new Integer(8);
+	public final static Integer ENTRADA_PARCELAMENTO = new Integer(9);
+	public final static Integer DOACOES = new Integer(10);
+	public final static Integer CANCELAMENTO_PARCELAMENTO = new Integer(11);
 
-    /** nullable persistent field */
-    private String descricaoAbreviada;
+	public final static Short INDICADOR_INCLUSAO_SIM = new Short("1");
+	public final static Short INDICADOR_INCLUSAO_NAO = new Short("2");
 
-    /** nullable persistent field */
-    private Short indicadorUso;
-    
-    private Short indicadorInclusao;
+	private Integer id;
 
-    /** nullable persistent field */
-    private Date ultimaAlteracao;
-    
-	public String[] retornaCamposChavePrimaria(){
+	private String descricao;
+
+	private String descricaoAbreviada;
+
+	private Short indicadorUso;
+
+	private Short indicadorInclusao;
+
+	private Date ultimaAlteracao;
+
+	public String[] retornaCamposChavePrimaria() {
 		String[] retorno = new String[1];
 		retorno[0] = "id";
 		return retorno;
-	}      
-    
-    /** full constructor */
-    public FinanciamentoTipo(String descricao, String descricaoAbreviada, Short indicadorUso, Date ultimaAlteracao) {
-        this.descricao = descricao;
-        this.descricaoAbreviada = descricaoAbreviada;
-        this.indicadorUso = indicadorUso;
-        this.ultimaAlteracao = ultimaAlteracao;
-    }
+	}
 
-    /** default constructor */
-    public FinanciamentoTipo() {
-    }
+	public FinanciamentoTipo(String descricao, String descricaoAbreviada, Short indicadorUso, Date ultimaAlteracao) {
+		this.descricao = descricao;
+		this.descricaoAbreviada = descricaoAbreviada;
+		this.indicadorUso = indicadorUso;
+		this.ultimaAlteracao = ultimaAlteracao;
+	}
 
-    public Integer getId() {
-        return this.id;
-    }
+	public FinanciamentoTipo() {
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public FinanciamentoTipo(Integer id) {
+		super();
+		this.id = id;
+	}
 
-    public String getDescricao() {
-        return this.descricao;
-    }
+	public Integer getId() {
+		return this.id;
+	}
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getDescricaoAbreviada() {
-        return this.descricaoAbreviada;
-    }
+	public String getDescricao() {
+		return this.descricao;
+	}
 
-    public void setDescricaoAbreviada(String descricaoAbreviada) {
-        this.descricaoAbreviada = descricaoAbreviada;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-    public Short getIndicadorUso() {
-        return this.indicadorUso;
-    }
+	public String getDescricaoAbreviada() {
+		return this.descricaoAbreviada;
+	}
 
-    public void setIndicadorUso(Short indicadorUso) {
-        this.indicadorUso = indicadorUso;
-    }
+	public void setDescricaoAbreviada(String descricaoAbreviada) {
+		this.descricaoAbreviada = descricaoAbreviada;
+	}
 
-    public Date getUltimaAlteracao() {
-        return this.ultimaAlteracao;
-    }
+	public Short getIndicadorUso() {
+		return this.indicadorUso;
+	}
 
-    public void setUltimaAlteracao(Date ultimaAlteracao) {
-        this.ultimaAlteracao = ultimaAlteracao;
-    }
+	public void setIndicadorUso(Short indicadorUso) {
+		this.indicadorUso = indicadorUso;
+	}
 
-    public Short getIndicadorInclusao() {
+	public Date getUltimaAlteracao() {
+		return this.ultimaAlteracao;
+	}
+
+	public void setUltimaAlteracao(Date ultimaAlteracao) {
+		this.ultimaAlteracao = ultimaAlteracao;
+	}
+
+	public Short getIndicadorInclusao() {
 		return indicadorInclusao;
 	}
 
@@ -113,15 +109,12 @@ public class FinanciamentoTipo extends ObjetoTransacao {
 	}
 
 	public String toString() {
-        return new ToStringBuilder(this)
-            .append("id", getId())
-            .toString();
-    }
+		return new ToStringBuilder(this).append("id", getId()).toString();
+	}
 
-	public Filtro retornaFiltro(){
+	public Filtro retornaFiltro() {
 		FiltroFinanciamentoTipo filtro = new FiltroFinanciamentoTipo();
-		filtro. adicionarParametro(
-				new ParametroSimples(FiltroFinanciamentoTipo.ID, this.getId()));		
+		filtro.adicionarParametro(new ParametroSimples(FiltroFinanciamentoTipo.ID, this.getId()));
 		return filtro;
 	}
 
@@ -129,7 +122,7 @@ public class FinanciamentoTipo extends ObjetoTransacao {
 	public void initializeLazy() {
 		retornaCamposChavePrimaria();
 	}
-	
+
 	@Override
 	public String getDescricaoParaRegistroTransacao() {
 		return getDescricao();
