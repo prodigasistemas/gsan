f04040a7c20fa6074c3260a0b3e987b7f03618c9
Pamela Gatinho <pamelagatinho@gmail.com>
2017-07-04 12:06:01 -0300

    Não permitindo incluir imóveis com algumas situacoes especiais de faturamento para a cobranca por telemarketing

--- src/gcom/cobranca/repositorios/IRepositorioCobrancaPorResultadoHBM.java
+++ src/gcom/cobranca/repositorios/IRepositorioCobrancaPorResultadoHBM.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface IRepositorioCobrancaPorResultadoHBM {
 
-	public Collection<Object[]> pesquisarQuantidadeContas(ComandoEmpresaCobrancaContaHelper helper, boolean agrupadoPorImovel) throws ErroRepositorioException;
+	public Collection<Object[]> pesquisarQuantidadeContas(ComandoEmpresaCobrancaContaHelper helper, boolean agrupadoPorImovel, Integer referenciaAtual) throws ErroRepositorioException;
 	
-	public List<Integer> pesquisarImoveis(ComandoEmpresaCobrancaContaHelper helper, boolean agrupadoPorImovel, boolean percentualInformado) throws ErroRepositorioException;
+	public List<Integer> pesquisarImoveis(ComandoEmpresaCobrancaContaHelper helper, boolean agrupadoPorImovel, boolean percentualInformado, Integer referenciaAtual) throws ErroRepositorioException;
 }
