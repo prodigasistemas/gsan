7855630647efc35428bf1a8db918c138250af31f
Felipe <felipesantos2089@gmail.com>
2018-07-05 16:59:41 -0300

    finalizando mapeamento da funcionalidade de treinamentos

--- src/gcom/gui/treinamentos/ExibirTreinamentosAction.java
+++ src/gcom/gui/treinamentos/ExibirTreinamentosAction.java
@@ -3,6 +3,7 @@ package gcom.gui.treinamentos;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
@@ -10,9 +11,7 @@ import gcom.gui.GcomAction;
 
 public class ExibirTreinamentosAction extends GcomAction {
 
-	public ActionForward execute(ActionMapping mapping, HttpServletRequest request, HttpServletResponse response) {
+	public ActionForward execute(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) {
 		return mapping.findForward("exibir");
 	}
-
-	
-	}
+}
