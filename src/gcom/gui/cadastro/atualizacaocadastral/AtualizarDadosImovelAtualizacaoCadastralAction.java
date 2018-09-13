011890f870dff4c59956e80c1f5c558e88b62f1e
Felipe Santos <felipesantos2089@gmail.com>
2018-09-13 17:59:11 -0300

    Alteração para permitir aprovar imóvel em fiscalização com ocorrência que não valida retorno do Recadastramento

--- src/gcom/gui/cadastro/atualizacaocadastral/AtualizarDadosImovelAtualizacaoCadastralAction.java
+++ src/gcom/gui/cadastro/atualizacaocadastral/AtualizarDadosImovelAtualizacaoCadastralAction.java
@@ -3,7 +3,6 @@ package gcom.gui.cadastro.atualizacaocadastral;
 import gcom.atualizacaocadastral.ImovelControleAtualizacaoCadastral;
 import gcom.cadastro.SituacaoAtualizacaoCadastral;
 import gcom.cadastro.imovel.CadastroOcorrencia;
-import gcom.fachada.Fachada;
 import gcom.gui.ActionServletException;
 import gcom.gui.GcomAction;
 import gcom.seguranca.acesso.usuario.Usuario;
@@ -11,7 +10,6 @@ import gcom.util.ConstantesSistema;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -19,85 +17,82 @@ import org.apache.struts.action.ActionMapping;
 
 public class AtualizarDadosImovelAtualizacaoCadastralAction extends GcomAction {
 
-	private Fachada fachada = Fachada.getInstancia();
-	
-    public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-    	ActionForward retorno =	actionMapping.findForward("filtrarAlteracaoAtualizacaoCadastral");
-        
+	public ActionForward execute(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ExibirAtualizarDadosImovelAtualizacaoCadastralPopupActionForm form = (ExibirAtualizarDadosImovelAtualizacaoCadastralPopupActionForm) actionForm;
 
-        HttpSession sessao = httpServletRequest.getSession(false);
-		
-		Usuario usuario = (Usuario) sessao.getAttribute("usuarioLogado");
-		
-		ImovelControleAtualizacaoCadastral imovelControle = fachada.pesquisarImovelControleAtualizacao(new Integer(form.getIdImovel()));
-		
-		CadastroOcorrencia cadastroOcorrencia = imovelControle.getCadastroOcorrencia();
-		
-		if (cadastroOcorrencia != null) {
-			if (cadastroOcorrencia.getIndicadorValidacao().equals(ConstantesSistema.SIM)) {
-				
-				if (isImovelFiscalizado(imovelControle)) {
-					
+		ImovelControleAtualizacaoCadastral controle = getFachada().pesquisarImovelControleAtualizacao(new Integer(form.getIdImovel()));
+		CadastroOcorrencia ocorrencia = controle.getCadastroOcorrencia();
+
+		if (ocorrencia != null) {
+			if (permiteAprovarOuConcluirFiscalizacao(controle, ocorrencia.getIndicadorValidacao())) {
+
+				if (isImovelFiscalizado(controle)) {
 					String registrosSelecionados = null;
-					
-					if (imovelControle.isEmFiscalizacao()) {
+
+					if (controle.isEmFiscalizacao()) {
 						registrosSelecionados = form.getIdRegistrosFiscalizados();
 					} else {
 						registrosSelecionados = form.getIdRegistrosAutorizados();
 					}
-					
+
 					if (!registrosSelecionados.equals("")) {
-						
 						String[] listaIdRegistrosSim = registrosSelecionados.split(",");
-						
+
 						if (listaIdRegistrosSim != null && !listaIdRegistrosSim.equals("")) {
-							fachada.atualizarIndicadorAutorizacaoColunaAtualizacaoCadastral(Integer.valueOf(form.getIdImovel()),
-									listaIdRegistrosSim, ConstantesSistema.SIM, usuario, obterCampoParaAtualizar(imovelControle));
-							
-							this.atualizarSituacaoImovel(imovelControle);
+							getFachada().atualizarIndicadorAutorizacaoColunaAtualizacaoCadastral(Integer.valueOf(form.getIdImovel()), listaIdRegistrosSim, ConstantesSistema.SIM,
+									(Usuario) request.getSession(false).getAttribute("usuarioLogado"), obterCampoParaAtualizar(controle));
+
+							this.atualizarSituacaoImovel(controle);
 						}
 					}
-					
-					httpServletRequest.setAttribute("reload", true);
+
+					request.setAttribute("reload", true);
 				} else {
 					throw new ActionServletException("atencao.imovel_nao_fiscalizado", "");
 				}
 			} else {
-				throw new ActionServletException("atencao.cadastro_ocorrencia_invalido", cadastroOcorrencia.getDescricao());
+				throw new ActionServletException("atencao.cadastro_ocorrencia_invalido", ocorrencia.getDescricao());
 			}
 		} else {
 			throw new ActionServletException("atencao.cadastro_ocorrencia_invalido", "NULO. Carregue o imóvel novamente");
 		}
-		
-        return retorno;
-    }
-	
+
+		return mapping.findForward("filtrarAlteracaoAtualizacaoCadastral");
+	}
+
 	private void atualizarSituacaoImovel(ImovelControleAtualizacaoCadastral imovelControle) {
 		if (imovelControle.isEmFiscalizacao()) {
-			fachada.atualizarSituacaoImovelControle(imovelControle.getImovel().getId(), SituacaoAtualizacaoCadastral.FISCALIZADO);
+			getFachada().atualizarSituacaoImovelControle(imovelControle.getImovel().getId(), SituacaoAtualizacaoCadastral.FISCALIZADO);
 		} else if (imovelControle.isPreAprovado() || imovelControle.isFiscalizado()) {
-			fachada.atualizarSituacaoImovelControle(imovelControle.getImovel().getId(), SituacaoAtualizacaoCadastral.APROVADO);
+			getFachada().atualizarSituacaoImovelControle(imovelControle.getImovel().getId(), SituacaoAtualizacaoCadastral.APROVADO);
 		}
 	}
-	
+
 	private boolean isImovelFiscalizado(ImovelControleAtualizacaoCadastral imovelControle) {
 		boolean imovelFiscalizado = true;
-		
-		if (imovelControle.isEmFiscalizacao() && !fachada.possuiInformacoesFiscalizacao(imovelControle)) {
+
+		if (imovelControle.isEmFiscalizacao() && !getFachada().possuiInformacoesFiscalizacao(imovelControle)) {
 			imovelFiscalizado = false;
-		} 
-		
+		}
+
 		return imovelFiscalizado;
 	}
-	
+
 	private String obterCampoParaAtualizar(ImovelControleAtualizacaoCadastral imovelControle) {
-		String campo = ""; 
+		String campo = "";
 		if (imovelControle.isPreAprovado())
 			campo = "preaprovado";
-		
+
 		if (imovelControle.isFiscalizado())
 			campo = "fiscalizado";
 		return campo;
+	}
+	
+	private boolean permiteAprovarOuConcluirFiscalizacao(ImovelControleAtualizacaoCadastral controle, short indicadorValidacao) {
+		return (controle.isPreAprovado() && retornoValidado(indicadorValidacao)) || controle.isEmFiscalizacao();
+	}
+	
+	private boolean retornoValidado(short indicadorValidacao) {
+		return indicadorValidacao == ConstantesSistema.SIM.shortValue();
 	}
 }
