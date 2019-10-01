ce2545407b5bf698ee70d33994894deeedd0c28b
Felipe Santos <felipesantos2089@gmail.com>
2019-10-01 15:17:48 -0300

    Adicionando validação nos builds de cliente proprietário e responsável do Recadastramento

--- src/gcom/cadastro/cliente/ClienteProprietarioBuilder.java
+++ src/gcom/cadastro/cliente/ClienteProprietarioBuilder.java
@@ -1,41 +1,43 @@
 package gcom.cadastro.cliente;
 
+import org.apache.commons.lang.StringUtils;
+
 import gcom.cadastro.atualizacaocadastral.command.AtualizacaoCadastralImovel;
 import gcom.cadastro.endereco.EnderecoTipo;
 
-import org.apache.commons.lang.StringUtils;
-
 public class ClienteProprietarioBuilder extends ClienteBuilder {
-	public ClienteProprietarioBuilder(AtualizacaoCadastralImovel atualizacaoCadastralImovel){
+	public ClienteProprietarioBuilder(AtualizacaoCadastralImovel atualizacaoCadastralImovel) {
 		super(atualizacaoCadastralImovel);
 	}
 
 	public IClienteAtualizacaoCadastral buildCliente(Short clienteRelacaoTipo) {
 		buildCliente(PROPRIETARIO, clienteRelacaoTipo);
-		
+
 		String campo;
-		
+
 		clienteTxt.setDescricaoLogradouro(atualizacaoCadastralImovel.getLinhaCliente("logradouroProprietario"));
-		
+
 		campo = atualizacaoCadastralImovel.getLinhaCliente("idTipoLogradouroProprietario");
-		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)){
+		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)) {
 			clienteTxt.setIdLogradouroTipo(Integer.parseInt(campo) == 0 ? null : Integer.parseInt(campo));
 		}
 
 		clienteTxt.setNumeroImovel(atualizacaoCadastralImovel.getLinhaCliente("numeroProprietario"));
 		clienteTxt.setComplementoEndereco(atualizacaoCadastralImovel.getLinhaCliente("complementoProprietario"));
 		clienteTxt.setNomeBairro(atualizacaoCadastralImovel.getLinhaCliente("bairroProprietario"));
-		
+
 		campo = atualizacaoCadastralImovel.getLinhaCliente("cepProprietario");
-		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)){
+		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)) {
 			clienteTxt.setCodigoCep(Integer.parseInt(campo));
 		}
-		
+
 		clienteTxt.setNomeMunicipio(atualizacaoCadastralImovel.getLinhaCliente("municipioProprietario"));
-		
+
 		campo = atualizacaoCadastralImovel.getLinhaCliente("tipoEnderecoProprietario");
-		clienteTxt.setEnderecoTipo(new EnderecoTipo(Integer.parseInt(campo) == 0 ? null : Integer.parseInt(campo)));
-		
+		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)) {
+			clienteTxt.setEnderecoTipo(new EnderecoTipo(Integer.parseInt(campo) == 0 ? null : Integer.parseInt(campo)));
+		}
+
 		return clienteTxt;
 	}
 }
--- src/gcom/cadastro/cliente/ClienteResponsavelBuilder.java
+++ src/gcom/cadastro/cliente/ClienteResponsavelBuilder.java
@@ -5,39 +5,40 @@ import org.apache.commons.lang.StringUtils;
 import gcom.cadastro.atualizacaocadastral.command.AtualizacaoCadastralImovel;
 import gcom.cadastro.endereco.EnderecoTipo;
 
-public class ClienteResponsavelBuilder extends ClienteBuilder{
+public class ClienteResponsavelBuilder extends ClienteBuilder {
 
-	public ClienteResponsavelBuilder(AtualizacaoCadastralImovel atualizacaoCadastralImovel){
+	public ClienteResponsavelBuilder(AtualizacaoCadastralImovel atualizacaoCadastralImovel) {
 		super(atualizacaoCadastralImovel);
 	}
-	
 
 	public IClienteAtualizacaoCadastral buildCliente(Short clienteRelacaoTipo) {
 		buildCliente(RESPONSAVEL, clienteRelacaoTipo);
-		
+
 		String campo;
-		
+
 		clienteTxt.setDescricaoLogradouro(atualizacaoCadastralImovel.getLinhaCliente("logradouroResponsavel"));
-		
+
 		campo = atualizacaoCadastralImovel.getLinhaCliente("idTipoLogradouroResponsavel");
-		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)){
+		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)) {
 			clienteTxt.setIdLogradouroTipo(Integer.parseInt(campo) == 0 ? null : Integer.parseInt(campo));
 		}
-		
+
 		clienteTxt.setNumeroImovel(atualizacaoCadastralImovel.getLinhaCliente("numeroResponsavel"));
 		clienteTxt.setComplementoEndereco(atualizacaoCadastralImovel.getLinhaCliente("complementoResponsavel"));
 		clienteTxt.setNomeBairro(atualizacaoCadastralImovel.getLinhaCliente("bairroResponsavel"));
-		
+
 		campo = atualizacaoCadastralImovel.getLinhaCliente("cepResponsavel");
-		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)){
+		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)) {
 			clienteTxt.setCodigoCep(Integer.parseInt(campo));
 		}
-		
+
 		clienteTxt.setNomeMunicipio(atualizacaoCadastralImovel.getLinhaCliente("municipioResponsavel"));
-		
+
 		campo = atualizacaoCadastralImovel.getLinhaCliente("tipoEnderecoResponsavel");
-		clienteTxt.setEnderecoTipo(new EnderecoTipo(Integer.parseInt(campo) == 0 ? null : Integer.parseInt(campo)));
-		
+		if (StringUtils.isNotEmpty(campo) && StringUtils.isNumeric(campo)) {
+			clienteTxt.setEnderecoTipo(new EnderecoTipo(Integer.parseInt(campo) == 0 ? null : Integer.parseInt(campo)));
+		}
+
 		return clienteTxt;
 	}
 }
