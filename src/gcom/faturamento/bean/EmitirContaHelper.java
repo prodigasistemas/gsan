156bf066fec56eb8d35dcc1eec4ea01fed14a914
Pamela Gatinho <pamelagatinho@gmail.com>
2021-10-04 15:02:34 -0300

    Ajuste no helper ao setar mensagem do bolsa agua

--- src/gcom/faturamento/bean/EmitirContaHelper.java
+++ src/gcom/faturamento/bean/EmitirContaHelper.java
@@ -2002,4 +2002,12 @@ public class EmitirContaHelper implements Serializable {
 	public void setMensagemBolsaAgua(String mensagemBolsaAgua) {
 		this.mensagemBolsaAgua = mensagemBolsaAgua;
 	}	
+	
+	public void preencherMsgBolsaAgua() {
+		if (this.valorConta.doubleValue() > 0 && this.valorCreditoBolsaAgua.doubleValue() > 0) {
+			this.mensagemBolsaAgua = "Programa Água Pará 20.000 litros quitados pelo Governo do Estado do Pará";
+		} else {
+			this.mensagemBolsaAgua = "";
+		}
+	}
 }
